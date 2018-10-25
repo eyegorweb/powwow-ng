4252f25f541edcd93fd3f40e14738e92675a54cc
Eduardo San Martin Morote <posva13@gmail.com>
2018-10-25 12:15:31 +0200

    chore: move tests to same place

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,8 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   snapshotSerializers: ['jest-serializer-vue'],
-  testMatch: ['**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)|**/src/**/*.spec.(js|jsx|ts|tsx)',
+  ],
   testURL: 'http://localhost/',
 };
--- src/components/HelloWorld.spec.js
+++ src/components/HelloWorld.spec.js
@@ -2,9 +2,11 @@ import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
+  /** @type {import('@vue/test-utils').Wrapper} */
+  let wrapper;
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
