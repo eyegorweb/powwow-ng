3296e9e2eeb16b8e2d5a9a0373b19ae7344c18cb
Marwane <marwane.belahcen3@docapost.fr>
2018-10-26 12:00:46 +0200

    Configurer storybook

--- config/storybook/addons.js
+++ config/storybook/addons.js
@@ -1,3 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import '@storybook/addon-actions/register'
-import '@storybook/addon-links/register'
+import '@storybook/addon-storysource/register';
+import '@storybook/addon-actions/register';
+import '@storybook/addon-links/register';
+
+// import '@storybook/addon-viewport/register';
+/*
+import '@storybook/addon-notes/register';
+import '@storybook/addon-knobs/register';
+import '@storybook/addon-options/register';
+import '@storybook/addon-backgrounds/register';
+//*/
