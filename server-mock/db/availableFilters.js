424c833ea0c6f5aaed35b9850676d7b6a127cea5
igor.trifunovic3@docapost.fr <igor.trifunovic3@docapost.fr>
2018-12-17 17:04:57 +0100

    Display possible filters

--- server-mock/db/availableFilters.js
+++ server-mock/db/availableFilters.js
@@ -3,7 +3,19 @@ module.exports = {
     {
       name: 'partners',
       title: 'Partenaires',
-      type: 'choices',
+      type: '',
+      values: [],
+    },
+    {
+      name: 'orderAccount',
+      title: 'Compte de facturation',
+      type: '',
+      values: [],
+    },
+    {
+      name: 'commandStatus',
+      title: 'Statut de la commande',
+      type: '',
       values: [
         {
           code: 'toValidate',
@@ -16,9 +28,63 @@ module.exports = {
       ],
     },
     {
+      name: 'commandReference',
+      title: 'Référence de la commande',
+      type: '',
+      values: [],
+    },
+    {
       name: 'orderDate',
       title: 'Date de la commande',
-      type: 'dateRange',
+      type: '',
+      values: [],
+    },
+    {
+      name: 'offer',
+      title: 'Offre',
+      type: '',
+      values: [],
+    },
+    {
+      name: 'quantity',
+      title: 'Qauntité',
+      type: '',
+      values: [],
+    },
+    {
+      name: 'personallabels',
+      title: 'Champs personnalisés',
+      type: '',
+      values: [],
+    },
+    {
+      name: 'offer',
+      title: 'Créateur de la commande',
+      type: 'searchithSelect',
+      values: [],
+    },
+    {
+      name: 'activated',
+      title: 'Activé',
+      type: '',
+      values: [],
+    },
+    {
+      name: 'country',
+      title: 'Pays de livraison',
+      type: '',
+      values: [],
+    },
+    {
+      name: 'zipcode',
+      title: 'Code postal',
+      type: '',
+      values: [],
+    },
+    {
+      name: 'city',
+      title: 'Ville',
+      type: '',
       values: [],
     },
   ],
