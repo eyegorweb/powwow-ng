4f78ccbcf51bbed30d04cb54fc5cc82ad13e55b5
Marwane <b.marwane@gmail.com>
2018-12-18 11:06:51 +0100

    Lint

--- src/components/GetSimFilters.vue
+++ src/components/GetSimFilters.vue
@@ -4,19 +4,19 @@
       <h5 class="card-title">FILTRES</h5>
       <draggable>
         <transition-group>
-          <FoldableBlock :title="'Partenaires'" :key="'el1'" draggable></FoldableBlock>
-          <FoldableBlock :title="'Compte de facturation'" :key="'el2'" draggable></FoldableBlock>
-          <FoldableBlock :title="'Statut de la commande'" :key="'el3'" draggable></FoldableBlock>
-          <FoldableBlock :title="'Référence de la commande'" :key="'el4'" draggable></FoldableBlock>
-          <FoldableBlock :title="'Date de la commande'" :key="'el5'" draggable></FoldableBlock>
-          <FoldableBlock :title="'Offre'" :key="'el6'" draggable></FoldableBlock>
-          <FoldableBlock :title="'Quantité'" :key="'el7'" draggable></FoldableBlock>
-          <FoldableBlock :title="'Champs personnalisés'" :key="'el8'" draggable></FoldableBlock>
-          <FoldableBlock :title="'Créateur de la commande'" :key="'el9'" draggable></FoldableBlock>
-          <FoldableBlock :title="'Activé'" :key="'el10'" draggable></FoldableBlock>
-          <FoldableBlock :title="'Pays de livraison'" :key="'el11'" draggable></FoldableBlock>
-          <FoldableBlock :title="'Code postal'" :key="'el12'" draggable></FoldableBlock>
-          <FoldableBlock :title="'Ville'" :key="'el13'" draggable></FoldableBlock>
+          <FoldableBlock :title="'Partenaires'" :key="'el1'" draggable />
+          <FoldableBlock :title="'Compte de facturation'" :key="'el2'" draggable />
+          <FoldableBlock :title="'Statut de la commande'" :key="'el3'" draggable />
+          <FoldableBlock :title="'Référence de la commande'" :key="'el4'" draggable />
+          <FoldableBlock :title="'Date de la commande'" :key="'el5'" draggable />
+          <FoldableBlock :title="'Offre'" :key="'el6'" draggable />
+          <FoldableBlock :title="'Quantité'" :key="'el7'" draggable />
+          <FoldableBlock :title="'Champs personnalisés'" :key="'el8'" draggable />
+          <FoldableBlock :title="'Créateur de la commande'" :key="'el9'" draggable />
+          <FoldableBlock :title="'Activé'" :key="'el10'" draggable />
+          <FoldableBlock :title="'Pays de livraison'" :key="'el11'" draggable />
+          <FoldableBlock :title="'Code postal'" :key="'el12'" draggable />
+          <FoldableBlock :title="'Ville'" :key="'el13'" draggable />
         </transition-group>
       </draggable>
     </div>
