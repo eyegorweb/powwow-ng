db83890a175bc0b3df83a61bda3cbb9ea9a27b6b
igor.trifunovic3@docapost.fr <igor.trifunovic3@docapost.fr>
2018-12-18 10:50:37 +0100

    add vue

---
+++ src/components/GetSimFilters.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card filter-bar">
+    <div class="card-body">
+      <h5 class="card-title">FILTRES</h5>
+      <PossibleFilters :all-available-filters="allAvailableFilters" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+import draggable from 'vuedraggable';
+import PossibleFilters from '@/components/filters/choices/PossibleFilters';
+import FoldableBlock from '@/components/filters/choices/FoldableBlock';
+
+export default {
+  components: {
+    draggable,
+    FoldableBlock,
+    PossibleFilters,
+  },
+
+  created() {
+    this.loadPossibleFilters();
+  },
+
+  computed: {
+    ...mapGetters(['allAvailableFilters']),
+  },
+
+  methods: {
+    ...mapActions(['loadPossibleFilters']),
+  },
+};
+</script>
+
+<style scoped>
+</style>
