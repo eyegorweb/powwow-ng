46c7f7c61adc9c7db78b30cf077d36dbcf5a8141
olirabt <olirabt@gmail.com>
2019-01-11 15:48:23 +0100

    add lien pour voir tous les types de cartes sim

--- src/components/GetSimSelectSimType.vue
+++ src/components/GetSimSelectSimType.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <h2 class="title">{{ $t("getsim.manage-orders") }}</h2>
-    <GetSimTypeOption v-for="item in test"/>
+    <div class="choose-sim-type">
+      <GetSimTypeOption v-for="item in test"/>
+      <div class="text-right">
+        <a href="#" class="show-all-types text-right">
+          {{ $t("getsim.show-all-sim-types") }}
+          <i data-v-0d262568 class="arrow ic-Arrow-Down-Icon"></i>
+        </a>
+      </div>
+      <p>encore du texte</p>
+    </div>
   </div>
 </template>
 
@@ -12,7 +21,7 @@ export default {
   name: 'GetSimSelectSimType',
   data() {
     return {
-      test: 2,
+      test: 3,
     };
   },
   components: {
@@ -21,7 +30,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .title {
   background-color: transparent;
   color: $dark-gray;
@@ -30,5 +39,23 @@ export default {
   margin: 50px 0 30px;
   padding: 0;
   text-align: center;
+}
+.show-all-types {
+  color: $secondary;
+  font-weight: 600;
+  font-size: 0.9rem;
+  appearance: none;
+  padding: 0;
+  margin: 0;
+  border: none;
+  background-color: transparent;
+
+  &:hover {
+    text-decoration: none;
+  }
+
+  i {
+    font-size: 1rem;
+  }
 }
 </style>
