f71dac15d7b0081ceb22d478c282b3c0e3a546ae
olirabt <olirabt@gmail.com>
2019-01-11 11:53:06 +0100

    design d'un type de sim

--- src/components/GetSimTypeOption.vue
+++ src/components/GetSimTypeOption.vue
@@ -1,14 +1,61 @@
 <template>
-  <div>
-    <p>Un test</p>
+  <div class="simtype">
+    <div class="simtype__item">
+      <UiCheckbox shape="round" v-model="model"/>
+    </div>
+    <div class="simtype__item">
+      <p class="simtype__name m-0">SIM M2M Endurcie 076</p>
+      <p class="simtype__format m-0">Format : Standard, sans PIN</p>
+      <p class="simtype__functions m-0">Brevets et fonctionnalités : Best network + Smart Roaming</p>
+      <p class="simtype__order-status m-0">Dernière commande : 27/09/18</p>
+    </div>
+    <div class="simtype__item">
+      <img src="@/assets/simtype.png">
+    </div>
   </div>
 </template>
 
 <script>
+import UiCheckbox from '@/components/ui/Checkbox';
+
 export default {
   name: 'GetSimTypeOption',
+  computed: {
+    model: {
+      get: ({ checked }) => checked,
+      set(model) {
+        this.$emit('update:checked', model);
+      },
+    },
+  },
+  components: {
+    UiCheckbox,
+  },
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.simtype {
+  border: 1px solid $medium-gray;
+  align-items: stretch;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  padding: 10px 20px 10px 15px;
+  margin-bottom: 10px;
+
+  p {
+    color: $dark-gray;
+    font-size: 0.8rem;
+    text-align: left;
+
+    &.simtype__name {
+      font-size: 0.9rem;
+    }
+
+    &.simtype__order-status {
+      color: $gray-680;
+    }
+  }
+}
 </style>
