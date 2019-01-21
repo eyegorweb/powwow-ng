5732c0100025643c79f328daf56fc0bbd9713b16
Igor Trifunovic <igor.trifunovic3@docapost.fr>
2019-01-21 15:59:26 +0100

    Feat/170/api users

---
+++ src/views/GetSim/GetSimOrderCreatorFilter.spec.js
@@ -0,0 +1,126 @@
+import { mount } from '@vue/test-utils';
+
+import * as api from '@/api/orderCreator';
+
+import { Store } from 'vuex-mock-store';
+import { $t } from '@/../tests-utils';
+
+import GetSimOrderCreatorFilter from './GetSimOrderCreatorFilter';
+
+const store = new Store({
+  getters: {
+    selectedOrderCreatorValues: [],
+    selectedPartnersValues: [
+      {
+        id: 'id_partner_1',
+      },
+    ],
+  },
+});
+
+afterEach(() => store.reset());
+
+const mocks = { $t, $store: store };
+
+describe('GetSimOrderCreatorFilter', () => {
+  it('fetches order creators when component is mounted', () => {
+    const creatorsData = [
+      {
+        label: 'Circum',
+        id: 19,
+      },
+      {
+        label: 'Xylar',
+        id: 20,
+      },
+    ];
+    api.fetchOrderCreators = jest.fn();
+    api.fetchOrderCreators.mockResolvedValue(creatorsData);
+
+    mount(GetSimOrderCreatorFilter, { mocks });
+
+    expect(api.fetchOrderCreators).toHaveBeenCalled();
+  });
+
+  it('searches for new values from api with selected partners', async () => {
+    const creatorsData = [
+      {
+        id: 1,
+        name: {
+          firstName: 'wistiti',
+          lastName: 'le lion',
+        },
+        party: {
+          id: 1,
+        },
+      },
+    ];
+    api.fetchOrderCreators = jest.fn();
+    api.fetchOrderCreators.mockResolvedValue(creatorsData);
+
+    const wrapper = mount(GetSimOrderCreatorFilter, { mocks });
+
+    await wrapper.vm.searchValueChanged('wistiti');
+
+    expect(wrapper.vm.creators).toEqual([
+      {
+        id: 1,
+        label: 'wistiti le lion',
+        partnerId: 1,
+      },
+    ]);
+
+    expect(api.fetchOrderCreators).toHaveBeenCalledWith(
+      'wistiti',
+      [
+        {
+          id: 'id_partner_1',
+        },
+      ],
+      { limit: 10, page: 0 }
+    );
+  });
+
+  it('adds next page content to current content', async () => {
+    const creatorsData = [
+      {
+        id: 1,
+        name: {
+          firstName: 'User',
+          lastName: '1',
+        },
+        party: {
+          id: 1,
+        },
+      },
+    ];
+    const offerData2 = [
+      {
+        id: 2,
+        name: {
+          firstName: 'User',
+          lastName: '2',
+        },
+        party: {
+          id: 1,
+        },
+      },
+    ];
+    api.fetchOrderCreators = jest.fn();
+    api.fetchOrderCreators.mockResolvedValue(creatorsData);
+
+    const wrapper = mount(GetSimOrderCreatorFilter, { mocks });
+
+    await wrapper.vm.searchValueChanged('wistiti');
+    api.fetchOrderCreators.mockResolvedValue(offerData2);
+
+    await wrapper.vm.nextPage();
+
+    expect(wrapper.vm.page).toEqual(1);
+
+    expect(wrapper.vm.creators).toEqual([
+      { id: 1, label: 'User 1', partnerId: 1 },
+      { id: 2, label: 'User 2', partnerId: 1 },
+    ]);
+  });
+});
