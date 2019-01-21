939a6de0e2d8ea4074583f2d087eb3dc33ecd075
olirabt <olirabt@gmail.com>
2019-01-21 10:08:09 +0100

    add test

--- src/views/GetSim/CreateOrder/CreateOrderStepProduct.spec.js
+++ src/views/GetSim/CreateOrder/CreateOrderStepProduct.spec.js
@@ -4,55 +4,55 @@ import CreateOrderStepProduct from './CreateOrderStepProduct';
 
 const mocks = { $i18n, $t };
 
+const products = [
+  {
+    id: '01',
+    name: 'SIM M2M Endurcie 076',
+    format: 'Standard, sans PIN',
+    patent: 'Best network + Smart Roaming',
+    lastCommand: '12/01/2018',
+  },
+  {
+    id: '02',
+    name: 'SIM M2M Endurcie 075',
+    format: 'Standard, sans PIN',
+    patent: 'Backup FR',
+    lastCommand: '12/01/2018',
+  },
+  {
+    id: '03',
+    name: 'SIM M2M Endurcie 024 (bobine de 3000)',
+    format: 'Soudée, sans PIN',
+    patent: 'Aucun',
+    lastCommand: '12/01/2018',
+  },
+  {
+    id: '04',
+    name: 'SIM M2M Endurcie 079',
+    format: 'Standard, sans PIN',
+    patent: 'Best network + Smart Roaming',
+    lastCommand: '12/01/2018',
+  },
+  {
+    id: '05',
+    name: 'SIM M2M Endurcie 096',
+    format: 'Standard, sans PIN',
+    patent: 'Backup FR',
+    lastCommand: '12/01/2018',
+  },
+  {
+    id: '06',
+    name: 'SIM M2M Endurcie 047 (bobine de 3000)',
+    format: 'Soudée, sans PIN',
+    patent: 'Aucun',
+    lastCommand: '12/01/2018',
+  },
+];
+
 describe('CreateOrderStepProduct.vue', () => {
   /** @type {import('@vue/test-utils').Wrapper} */
   let wrapper;
   beforeEach(() => {
-    const products = [
-      {
-        id: '01',
-        name: 'SIM M2M Endurcie 076',
-        format: 'Standard, sans PIN',
-        patent: 'Best network + Smart Roaming',
-        lastCommand: '12/01/2018',
-      },
-      {
-        id: '02',
-        name: 'SIM M2M Endurcie 075',
-        format: 'Standard, sans PIN',
-        patent: 'Backup FR',
-        lastCommand: '12/01/2018',
-      },
-      {
-        id: '03',
-        name: 'SIM M2M Endurcie 024 (bobine de 3000)',
-        format: 'Soudée, sans PIN',
-        patent: 'Aucun',
-        lastCommand: '12/01/2018',
-      },
-      {
-        id: '04',
-        name: 'SIM M2M Endurcie 079',
-        format: 'Standard, sans PIN',
-        patent: 'Best network + Smart Roaming',
-        lastCommand: '12/01/2018',
-      },
-      {
-        id: '05',
-        name: 'SIM M2M Endurcie 096',
-        format: 'Standard, sans PIN',
-        patent: 'Backup FR',
-        lastCommand: '12/01/2018',
-      },
-      {
-        id: '06',
-        name: 'SIM M2M Endurcie 047 (bobine de 3000)',
-        format: 'Soudée, sans PIN',
-        patent: 'Aucun',
-        lastCommand: '12/01/2018',
-      },
-    ];
-
     wrapper = mount(CreateOrderStepProduct, {
       mocks,
     });
@@ -62,6 +62,11 @@ describe('CreateOrderStepProduct.vue', () => {
 
   it('renders only the first 3 items when loaded', () => {
     expect(wrapper.findAll('.simtype')).toHaveLength(3);
+  });
+
+  it('displays all items when the link is clicked', () => {
+    wrapper.find('.show-all-types').trigger('click');
+    expect(wrapper.findAll('.simtype')).toHaveLength(products.length);
   });
 
   it('adds active class to the clicked item', () => {
