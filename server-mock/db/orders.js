bd7ffc3aa4229d227dd5f9dec9112d8b4150bb8d
Marwane <b.marwane@gmail.com>
2019-02-11 11:35:13 +0100

    Update graphQL api

--- server-mock/db/orders.js
+++ server-mock/db/orders.js
@@ -3,305 +3,389 @@ module.exports = {
     total: 33,
     items: [
       {
-        id: '201',
-        creationDate: '28-05-2018',
-        activationAsked: true,
-        status: 'CONFIRMED',
-        orderItems: [
+        "id": "201",
+        "creationDate": "28-05-2018",
+        "orderDate": "28-05-2018 15:58:56",
+        "activationAsked": true,
+        "status": "CONFIRMED",
+        "externalId": null,
+        "customerAccount": {
+          "code": "6.42661"
+        },
+        "orderItems": [
           {
-            quantity: 100,
-            orderedProduct: {
-              description: 'Parc 1 forfait',
-              code: 'LYRA_PARC1_FORFAIT',
-            },
-          },
-        ],
+            "quantity": 100,
+            "orderedProduct": {
+              "description": "Parc 1 forfait",
+              "code": "LYRA_PARC1_FORFAIT"
+            }
+          }
+        ]
       },
       {
-        id: '181',
-        creationDate: '23-03-2018',
-        activationAsked: false,
-        status: 'CONFIRMED',
-        orderItems: [
+        "id": "181",
+        "creationDate": "23-03-2018",
+        "orderDate": "23-03-2018 13:54:50",
+        "activationAsked": false,
+        "status": "CONFIRMED",
+        "externalId": null,
+        "customerAccount": null,
+        "orderItems": [
           {
-            quantity: 10,
-            orderedProduct: {
-              description: 'M2M sim sans code pin',
-              code: '3526350037765',
-            },
-          },
-        ],
+            "quantity": 10,
+            "orderedProduct": {
+              "description": "M2M sim sans code pin",
+              "code": "3526350037765"
+            }
+          }
+        ]
       },
       {
-        id: '161',
-        creationDate: '28-02-2018',
-        activationAsked: false,
-        status: 'CONFIRMED',
-        orderItems: [
+        "id": "161",
+        "creationDate": "28-02-2018",
+        "orderDate": "28-02-2018 10:10:03",
+        "activationAsked": false,
+        "status": "CONFIRMED",
+        "externalId": null,
+        "customerAccount": {
+          "code": "6.42661"
+        },
+        "orderItems": [
           {
-            quantity: 50,
-            orderedProduct: {
-              description: 'M2M sim sans code pin',
-              code: '3526350037765',
-            },
-          },
-        ],
+            "quantity": 50,
+            "orderedProduct": {
+              "description": "M2M sim sans code pin",
+              "code": "3526350037765"
+            }
+          }
+        ]
       },
       {
-        id: '143',
-        creationDate: '15-02-2018',
-        activationAsked: false,
-        status: 'TERMINATED',
-        orderItems: [
+        "id": "143",
+        "creationDate": "15-02-2018",
+        "orderDate": "15-02-2018 16:21:08",
+        "activationAsked": false,
+        "status": "TERMINATED",
+        "externalId": null,
+        "customerAccount": null,
+        "orderItems": [
           {
-            quantity: 1,
-            orderedProduct: {
-              description: 'M2M sim sans code pin',
-              code: '3526350037765',
-            },
-          },
-        ],
+            "quantity": 1,
+            "orderedProduct": {
+              "description": "M2M sim sans code pin",
+              "code": "3526350037765"
+            }
+          }
+        ]
       },
       {
-        id: '142',
-        creationDate: '15-02-2018',
-        activationAsked: false,
-        status: 'TERMINATED',
-        orderItems: [
+        "id": "142",
+        "creationDate": "15-02-2018",
+        "orderDate": "15-02-2018 15:40:33",
+        "activationAsked": false,
+        "status": "TERMINATED",
+        "externalId": null,
+        "customerAccount": {
+          "code": "TEST"
+        },
+        "orderItems": [
           {
-            quantity: 1,
-            orderedProduct: {
-              description: 'M2M sim sans code pin',
-              code: '3526350037765',
-            },
-          },
-        ],
+            "quantity": 1,
+            "orderedProduct": {
+              "description": "M2M sim sans code pin",
+              "code": "3526350037765"
+            }
+          }
+        ]
       },
       {
-        id: '141',
-        creationDate: '15-02-2018',
-        activationAsked: false,
-        status: 'CONFIRMED',
-        orderItems: [
+        "id": "141",
+        "creationDate": "15-02-2018",
+        "orderDate": "15-02-2018 13:58:24",
+        "activationAsked": false,
+        "status": "CONFIRMED",
+        "externalId": null,
+        "customerAccount": {
+          "code": "6.42661"
+        },
+        "orderItems": [
           {
-            quantity: 100,
-            orderedProduct: {
-              description: 'M2M sim antivol avec code pin bobine',
-              code: '3526350037871',
-            },
-          },
-        ],
+            "quantity": 100,
+            "orderedProduct": {
+              "description": "M2M sim antivol avec code pin bobine",
+              "code": "3526350037871"
+            }
+          }
+        ]
       },
       {
-        id: '122',
-        creationDate: '05-12-2017',
-        activationAsked: false,
-        status: 'TERMINATED',
-        orderItems: [
+        "id": "122",
+        "creationDate": "05-12-2017",
+        "orderDate": "05-12-2017 11:37:02",
+        "activationAsked": false,
+        "status": "TERMINATED",
+        "externalId": null,
+        "customerAccount": null,
+        "orderItems": [
           {
-            quantity: 1,
-            orderedProduct: {
-              description: 'M2M sim avec code pin',
-              code: '3526350029494',
-            },
-          },
-        ],
+            "quantity": 1,
+            "orderedProduct": {
+              "description": "M2M sim avec code pin",
+              "code": "3526350029494"
+            }
+          }
+        ]
       },
       {
-        id: '121',
-        creationDate: '05-12-2017',
-        activationAsked: false,
-        status: 'TERMINATED',
-        orderItems: [
+        "id": "121",
+        "creationDate": "05-12-2017",
+        "orderDate": "05-12-2017 09:48:08",
+        "activationAsked": false,
+        "status": "TERMINATED",
+        "externalId": null,
+        "customerAccount": null,
+        "orderItems": [
           {
-            quantity: 1,
-            orderedProduct: {
-              description: 'M2M sim sans code pin',
-              code: '3526350037765',
-            },
-          },
-        ],
+            "quantity": 1,
+            "orderedProduct": {
+              "description": "M2M sim sans code pin",
+              "code": "3526350037765"
+            }
+          }
+        ]
       },
       {
-        id: '101',
-        creationDate: '23-10-2017',
-        activationAsked: true,
-        status: 'CONFIRMED',
-        orderItems: [
+        "id": "101",
+        "creationDate": "23-10-2017",
+        "orderDate": "23-10-2017 09:47:33",
+        "activationAsked": true,
+        "status": "CONFIRMED",
+        "externalId": null,
+        "customerAccount": {
+          "code": "6.42662"
+        },
+        "orderItems": [
           {
-            quantity: 100,
-            orderedProduct: {
-              description: 'Parc 1 compteur',
-              code: 'LYRA_PARC1_COMPTEUR',
-            },
-          },
-        ],
+            "quantity": 100,
+            "orderedProduct": {
+              "description": "Parc 1 compteur",
+              "code": "LYRA_PARC1_COMPTEUR"
+            }
+          }
+        ]
       },
       {
-        id: '82',
-        creationDate: '15-09-2017',
-        activationAsked: false,
-        status: 'TERMINATED',
-        orderItems: [
+        "id": "82",
+        "creationDate": "15-09-2017",
+        "orderDate": "15-09-2017 15:06:26",
+        "activationAsked": false,
+        "status": "TERMINATED",
+        "externalId": null,
+        "customerAccount": null,
+        "orderItems": [
           {
-            quantity: 1,
-            orderedProduct: {
-              description: 'M2M sim avec code pin',
-              code: '3526350029494',
-            },
-          },
-        ],
+            "quantity": 1,
+            "orderedProduct": {
+              "description": "M2M sim avec code pin",
+              "code": "3526350029494"
+            }
+          }
+        ]
       },
       {
-        id: '81',
-        creationDate: '15-09-2017',
-        activationAsked: false,
-        status: 'TERMINATED',
-        orderItems: [
+        "id": "81",
+        "creationDate": "15-09-2017",
+        "orderDate": "15-09-2017 11:41:58",
+        "activationAsked": false,
+        "status": "TERMINATED",
+        "externalId": null,
+        "customerAccount": null,
+        "orderItems": [
           {
-            quantity: 1,
-            orderedProduct: {
-              description: 'M2M sim avec code pin',
-              code: '3526350029494',
-            },
-          },
-        ],
+            "quantity": 1,
+            "orderedProduct": {
+              "description": "M2M sim avec code pin",
+              "code": "3526350029494"
+            }
+          }
+        ]
       },
       {
-        id: '61',
-        creationDate: '12-06-2017',
-        activationAsked: true,
-        status: 'CONFIRMED',
-        orderItems: [
+        "id": "61",
+        "creationDate": "12-06-2017",
+        "orderDate": "12-06-2017 14:30:47",
+        "activationAsked": true,
+        "status": "CONFIRMED",
+        "externalId": null,
+        "customerAccount": {
+          "code": "TimerBV"
+        },
+        "orderItems": [
           {
-            quantity: 1,
-            orderedProduct: {
-              description: 'offre TimerBV',
-              code: 'TBV_2MO_FR',
-            },
-          },
-        ],
+            "quantity": 1,
+            "orderedProduct": {
+              "description": "offre TimerBV",
+              "code": "TBV_2MO_FR"
+            }
+          }
+        ]
       },
       {
-        id: '56',
-        creationDate: '08-06-2017',
-        activationAsked: false,
-        status: 'TERMINATED',
-        orderItems: [
+        "id": "56",
+        "creationDate": "08-06-2017",
+        "orderDate": "08-06-2017 14:55:00",
+        "activationAsked": false,
+        "status": "TERMINATED",
+        "externalId": null,
+        "customerAccount": null,
+        "orderItems": [
           {
-            quantity: 1,
-            orderedProduct: {
-              description: 'M2M sim sans code pin',
-              code: '3526350037765',
-            },
-          },
-        ],
+            "quantity": 1,
+            "orderedProduct": {
+              "description": "M2M sim sans code pin",
+              "code": "3526350037765"
+            }
+          }
+        ]
       },
       {
-        id: '55',
-        creationDate: '08-06-2017',
-        activationAsked: false,
-        status: 'TERMINATED',
-        orderItems: [
+        "id": "55",
+        "creationDate": "08-06-2017",
+        "orderDate": "08-06-2017 14:46:04",
+        "activationAsked": false,
+        "status": "TERMINATED",
+        "externalId": null,
+        "customerAccount": null,
+        "orderItems": [
           {
-            quantity: 1,
-            orderedProduct: {
-              description: 'M2M sim sans code pin',
-              code: '3526350037765',
-            },
-          },
-        ],
+            "quantity": 1,
+            "orderedProduct": {
+              "description": "M2M sim sans code pin",
+              "code": "3526350037765"
+            }
+          }
+        ]
       },
       {
-        id: '54',
-        creationDate: '08-06-2017',
-        activationAsked: true,
-        status: 'TERMINATED',
-        orderItems: [
+        "id": "54",
+        "creationDate": "08-06-2017",
+        "orderDate": "08-06-2017 11:32:12",
+        "activationAsked": true,
+        "status": "TERMINATED",
+        "externalId": null,
+        "customerAccount": {
+          "code": "TimerBV"
+        },
+        "orderItems": [
           {
-            quantity: 1,
-            orderedProduct: {
-              description: 'offre TimerBV',
-              code: 'TBV_2MO_FR',
-            },
-          },
-        ],
+            "quantity": 1,
+            "orderedProduct": {
+              "description": "offre TimerBV",
+              "code": "TBV_2MO_FR"
+            }
+          }
+        ]
       },
       {
-        id: '53',
-        creationDate: '08-06-2017',
-        activationAsked: true,
-        status: 'NOT_VALIDATED',
-        orderItems: [
+        "id": "53",
+        "creationDate": "08-06-2017",
+        "orderDate": "08-06-2017 11:22:28",
+        "activationAsked": true,
+        "status": "NOT_VALIDATED",
+        "externalId": null,
+        "customerAccount": {
+          "code": "TimerBV"
+        },
+        "orderItems": [
           {
-            quantity: 1,
-            orderedProduct: {
-              description: 'offre TimerBV',
-              code: 'TBV_2MO_FR',
-            },
-          },
-        ],
+            "quantity": 1,
+            "orderedProduct": {
+              "description": "offre TimerBV",
+              "code": "TBV_2MO_FR"
+            }
+          }
+        ]
       },
       {
-        id: '52',
-        creationDate: '08-06-2017',
-        activationAsked: true,
-        status: 'NOT_VALIDATED',
-        orderItems: [
+        "id": "52",
+        "creationDate": "08-06-2017",
+        "orderDate": "08-06-2017 10:52:41",
+        "activationAsked": true,
+        "status": "NOT_VALIDATED",
+        "externalId": null,
+        "customerAccount": {
+          "code": "TimerBV"
+        },
+        "orderItems": [
           {
-            quantity: 1,
-            orderedProduct: {
-              description: 'offre TimerBV',
-              code: 'TBV_2MO_FR',
-            },
-          },
-        ],
+            "quantity": 1,
+            "orderedProduct": {
+              "description": "offre TimerBV",
+              "code": "TBV_2MO_FR"
+            }
+          }
+        ]
       },
       {
-        id: '51',
-        creationDate: '08-06-2017',
-        activationAsked: true,
-        status: 'NOT_VALIDATED',
-        orderItems: [
+        "id": "51",
+        "creationDate": "08-06-2017",
+        "orderDate": "08-06-2017 10:39:48",
+        "activationAsked": true,
+        "status": "NOT_VALIDATED",
+        "externalId": null,
+        "customerAccount": {
+          "code": "TimerBV"
+        },
+        "orderItems": [
           {
-            quantity: 1,
-            orderedProduct: {
-              description: 'offre TimerBV',
-              code: 'TBV_2MO_FR',
-            },
-          },
-        ],
+            "quantity": 1,
+            "orderedProduct": {
+              "description": "offre TimerBV",
+              "code": "TBV_2MO_FR"
+            }
+          }
+        ]
       },
       {
-        id: '50',
-        creationDate: '08-06-2017',
-        activationAsked: true,
-        status: 'NOT_VALIDATED',
-        orderItems: [
+        "id": "50",
+        "creationDate": "08-06-2017",
+        "orderDate": "08-06-2017 10:36:33",
+        "activationAsked": true,
+        "status": "NOT_VALIDATED",
+        "externalId": null,
+        "customerAccount": {
+          "code": "TimerBV"
+        },
+        "orderItems": [
           {
-            quantity: 1,
-            orderedProduct: {
-              description: 'offre TimerBV',
-              code: 'TBV_2MO_FR',
-            },
-          },
-        ],
+            "quantity": 1,
+            "orderedProduct": {
+              "description": "offre TimerBV",
+              "code": "TBV_2MO_FR"
+            }
+          }
+        ]
       },
       {
-        id: '48',
-        creationDate: '08-06-2017',
-        activationAsked: true,
-        status: 'NOT_VALIDATED',
-        orderItems: [
+        "id": "48",
+        "creationDate": "08-06-2017",
+        "orderDate": "08-06-2017 09:00:20",
+        "activationAsked": true,
+        "status": "NOT_VALIDATED",
+        "externalId": null,
+        "customerAccount": {
+          "code": "TimerBV"
+        },
+        "orderItems": [
           {
-            quantity: 1,
-            orderedProduct: {
-              description: 'offre TimerBV',
-              code: 'TBV_2MO_FR',
-            },
-          },
-        ],
-      },
+            "quantity": 1,
+            "orderedProduct": {
+              "description": "offre TimerBV",
+              "code": "TBV_2MO_FR"
+            }
+          }
+        ]
+      }
     ],
   },
 };
