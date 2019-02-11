181b7e34764bf27088d5e8dbd14c5a77e353f396
Marwane <b.marwane@gmail.com>
2019-02-11 15:25:24 +0100

    Mock countries

---
+++ server-mock/db/countries.js
@@ -0,0 +1,229 @@
+module.exports = {
+  "countries": {
+    "total": 37,
+    "countries": [
+      {
+        "id": "13",
+        "code": "DE",
+        "nameFr": "Allemagne",
+        "nameEn": "Germany"
+      },
+      {
+        "id": "1",
+        "code": "AD",
+        "nameFr": "Andorre",
+        "nameEn": "Andorra"
+      },
+      {
+        "id": "2",
+        "code": "AT",
+        "nameFr": "Autriche",
+        "nameEn": "Austria"
+      },
+      {
+        "id": "3",
+        "code": "BE",
+        "nameFr": "Belgique",
+        "nameEn": "Belgium"
+      },
+      {
+        "id": "37",
+        "code": "BR",
+        "nameFr": "Brésil",
+        "nameEn": "Brazil"
+      },
+      {
+        "id": "4",
+        "code": "BG",
+        "nameFr": "Bulgarie",
+        "nameEn": "Bulgaria"
+      },
+      {
+        "id": "5",
+        "code": "CA",
+        "nameFr": "Canada",
+        "nameEn": "Canada"
+      },
+      {
+        "id": "36",
+        "code": "CL",
+        "nameFr": "Chilie",
+        "nameEn": "Chilli"
+      },
+      {
+        "id": "6",
+        "code": "CN",
+        "nameFr": "Chine",
+        "nameEn": "China"
+      },
+      {
+        "id": "7",
+        "code": "CY",
+        "nameFr": "Chypre",
+        "nameEn": "Cyprus"
+      },
+      {
+        "id": "9",
+        "code": "DK",
+        "nameFr": "Danemark",
+        "nameEn": "Denmark"
+      },
+      {
+        "id": "30",
+        "code": "ES",
+        "nameFr": "Espagne",
+        "nameEn": "Spain"
+      },
+      {
+        "id": "10",
+        "code": "EE",
+        "nameFr": "Estonie",
+        "nameEn": "Estonia"
+      },
+      {
+        "id": "35",
+        "code": "US",
+        "nameFr": "Etats-Unis",
+        "nameEn": "United States"
+      },
+      {
+        "id": "11",
+        "code": "FI",
+        "nameFr": "Finlande",
+        "nameEn": "Finland"
+      },
+      {
+        "id": "12",
+        "code": "FR",
+        "nameFr": "France",
+        "nameEn": "France"
+      },
+      {
+        "id": "14",
+        "code": "GR",
+        "nameFr": "Grèce",
+        "nameEn": "Greece"
+      },
+      {
+        "id": "15",
+        "code": "HU",
+        "nameFr": "Hongrie",
+        "nameEn": "Hungary"
+      },
+      {
+        "id": "16",
+        "code": "IE",
+        "nameFr": "Irlande",
+        "nameEn": "Ireland"
+      },
+      {
+        "id": "17",
+        "code": "IT",
+        "nameFr": "Italie",
+        "nameEn": "Italy"
+      },
+      {
+        "id": "18",
+        "code": "LV",
+        "nameFr": "Lettonie",
+        "nameEn": "Latvia"
+      },
+      {
+        "id": "19",
+        "code": "LI",
+        "nameFr": "Liechtenstein",
+        "nameEn": "Liechtenstein"
+      },
+      {
+        "id": "20",
+        "code": "LT",
+        "nameFr": "Lituanie",
+        "nameEn": "Lithuania"
+      },
+      {
+        "id": "21",
+        "code": "LU",
+        "nameFr": "Luxembourg",
+        "nameEn": "Luxembourg"
+      },
+      {
+        "id": "22",
+        "code": "MT",
+        "nameFr": "Malte",
+        "nameEn": "Malta"
+      },
+      {
+        "id": "24",
+        "code": "NO",
+        "nameFr": "Norvège",
+        "nameEn": "Norway"
+      },
+      {
+        "id": "23",
+        "code": "NL",
+        "nameFr": "Pays-Bas",
+        "nameEn": "Netherlands"
+      },
+      {
+        "id": "25",
+        "code": "PL",
+        "nameFr": "Pologne",
+        "nameEn": "Poland"
+      },
+      {
+        "id": "26",
+        "code": "PT",
+        "nameFr": "Portugal",
+        "nameEn": "Portugal"
+      },
+      {
+        "id": "27",
+        "code": "RO",
+        "nameFr": "Roumanie",
+        "nameEn": "Romania"
+      },
+      {
+        "id": "34",
+        "code": "UK",
+        "nameFr": "Royaume-Uni",
+        "nameEn": "United Kingdom"
+      },
+      {
+        "id": "8",
+        "code": "CZ",
+        "nameFr": "République tchèque",
+        "nameEn": "Czech Republic"
+      },
+      {
+        "id": "28",
+        "code": "SK",
+        "nameFr": "Slovaquie",
+        "nameEn": "Slovakia"
+      },
+      {
+        "id": "29",
+        "code": "SI",
+        "nameFr": "Slovénie",
+        "nameEn": "Slovenia"
+      },
+      {
+        "id": "32",
+        "code": "CH",
+        "nameFr": "Suisse",
+        "nameEn": "Switzerland"
+      },
+      {
+        "id": "31",
+        "code": "SE",
+        "nameFr": "Suède",
+        "nameEn": "Sweden"
+      },
+      {
+        "id": "33",
+        "code": "TN",
+        "nameFr": "Tunisie",
+        "nameEn": "Tunisia"
+      }
+    ]
+  }
+}
--- server-mock/db/index.js
+++ server-mock/db/index.js
@@ -2,10 +2,12 @@ const orders = require('./orders');
 const getCurrentUser = require('./getCurrentUser');
 const indicators = require('./indicators');
 const partys = require('./partys');
+const countries = require('./countries');
 
 module.exports = {
   ...orders,
   ...getCurrentUser,
   ...indicators,
   ...partys,
+  ...countries,
 };
