cd73ecf31474f130ac2214fb855f9f7ca3dfc172
Marwane <b.marwane@gmail.com>
2019-02-13 10:37:17 +0100

    Add button

--- src/views/GetSim/CreateOrder/CreateOrderPanelSynthesis.vue
+++ src/views/GetSim/CreateOrder/CreateOrderPanelSynthesis.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="new-order-synthesis">
     <h6>{{ $t('orders.new.synthesis') }}</h6>
-    <div>
+    <div class="synthesis-content">
       <GetSimCreateOrderPanelSynthesisItem :item="synthesis.billingAccount" />
       <GetSimCreateOrderPanelSynthesisItem :item="synthesis.product" />
       <GetSimCreateOrderPanelSynthesisItem :item="synthesis.quantity" />
@@ -9,15 +9,28 @@
       <GetSimCreateOrderPanelSynthesisItem :item="synthesis.delivery" />
       <GetSimCreateOrderPanelSynthesisItem :item="synthesis.settings" />
     </div>
+    <div class="footer pt-3">
+      <UiCheckbox
+        v-model="accept"
+        :disabled="!synthesis.settings"
+      >{{ $t('orders.new.acceptConditions') }}</UiCheckbox>
+      <button
+        type="button"
+        class="btn btn-accent btn-lg btn-block"
+        :disabled="!accept"
+      >{{ $t('orders.new.acceptConditions') }}</button>
+    </div>
   </div>
 </template>
 
 <script>
 import GetSimCreateOrderPanelSynthesisItem from './CreateOrderPanelSynthesisItem';
+import UiCheckbox from '@/components/ui/Checkbox';
 
 export default {
   components: {
     GetSimCreateOrderPanelSynthesisItem,
+    UiCheckbox,
   },
   props: {
     synthesis: {
@@ -25,14 +38,51 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      accept: false,
+    };
+  },
 };
 </script>
 
 <style scoped lang="scss">
 .new-order-synthesis {
   padding: 1em !important;
+  display: flex;
+  flex-direction: column;
+  .synthesis-content {
+    flex: 1;
+    overflow-y: auto;
+  }
   h6 {
     margin-bottom: 2em;
+  }
+  .footer {
+    border-top: 1px solid $gray-400;
+    button {
+      font-size: 1rem;
+    }
+  }
+}
+
+@media screen and (max-height: 768px) {
+  .new-order-synthesis {
+    div.synthesis-content {
+      max-height: 23vh;
+      min-height: 70vh;
+    }
+    .footer /deep/ label {
+      font-size: 0.8rem;
+    }
+  }
+}
+@media screen and (min-height: 769px) {
+  .new-order-synthesis {
+    div.synthesis-content {
+      max-height: 23vh;
+      min-height: 73vh;
+    }
   }
 }
 </style>
