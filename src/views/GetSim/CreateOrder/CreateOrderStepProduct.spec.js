56f59ffdb6cb137766789105739c0d0ce5525ae8
Marwane <b.marwane@gmail.com>
2019-02-14 15:05:35 +0100

    Fix orders

--- src/views/GetSim/CreateOrder/CreateOrderStepProduct.spec.js
+++ src/views/GetSim/CreateOrder/CreateOrderStepProduct.spec.js
@@ -8,42 +8,42 @@ const mocks = { $i18n, $t };
 const products = [
   {
     id: '01',
-    name: 'SIM M2M Endurcie 076',
+    type: 'SIM M2M Endurcie 076',
     format: 'Standard, sans PIN',
     patent: 'Best network + Smart Roaming',
     lastCommand: '12/01/2018',
   },
   {
     id: '02',
-    name: 'SIM M2M Endurcie 075',
+    type: 'SIM M2M Endurcie 075',
     format: 'Standard, sans PIN',
     patent: 'Backup FR',
     lastCommand: '12/01/2018',
   },
   {
     id: '03',
-    name: 'SIM M2M Endurcie 024 (bobine de 3000)',
+    type: 'SIM M2M Endurcie 024 (bobine de 3000)',
     format: 'Soudée, sans PIN',
     patent: 'Aucun',
     lastCommand: '12/01/2018',
   },
   {
     id: '04',
-    name: 'SIM M2M Endurcie 079',
+    type: 'SIM M2M Endurcie 079',
     format: 'Standard, sans PIN',
     patent: 'Best network + Smart Roaming',
     lastCommand: '12/01/2018',
   },
   {
     id: '05',
-    name: 'SIM M2M Endurcie 096',
+    type: 'SIM M2M Endurcie 096',
     format: 'Standard, sans PIN',
     patent: 'Backup FR',
     lastCommand: '12/01/2018',
   },
   {
     id: '06',
-    name: 'SIM M2M Endurcie 047 (bobine de 3000)',
+    type: 'SIM M2M Endurcie 047 (bobine de 3000)',
     format: 'Soudée, sans PIN',
     patent: 'Aucun',
     lastCommand: '12/01/2018',
@@ -54,7 +54,7 @@ const selectedObject = {
   product: {
     label: 'common.product',
     value: {
-      content: ['SIM M2M Endurcie 076', 'Standard, sans PIN', 'Best network + Smart Roaming'],
+      content: ['SIM M2M Endurcie 076'],
       id: '01',
     },
   },
--- src/views/GetSim/CreateOrder/GetSimTypeOption.spec.js
+++ src/views/GetSim/CreateOrder/GetSimTypeOption.spec.js
@@ -5,11 +5,10 @@ import GetSimTypeOption from './GetSimTypeOption';
 const mocks = { $i18n, $t };
 
 const simType = {
-  id: '02',
-  name: 'SIM M2M Endurcie 075',
-  format: 'Standard, sans PIN',
-  patent: 'Backup FR',
-  lastCommand: '12/01/2018',
+  id: 76,
+  iccid: '8933215898343692617',
+  type: 'M2M sim antivol avec code pin',
+  accessPoint: null,
 };
 
 describe('GetSimTypeOption.vue', () => {
@@ -23,10 +22,7 @@ describe('GetSimTypeOption.vue', () => {
   });
 
   it('lists data passed in props', () => {
-    expect(wrapper.find('.simtype__name').text()).toContain(simType.name);
-    expect(wrapper.find('.simtype__format').text()).toContain(simType.format);
-    expect(wrapper.find('.simtype__functions').text()).toContain(simType.patent);
-    expect(wrapper.find('.simtype__order-status').text()).toContain(simType.lastCommand);
+    expect(wrapper.find('.simtype__name').text()).toContain(simType.type);
   });
 
   it('emits an event with the right data', () => {
