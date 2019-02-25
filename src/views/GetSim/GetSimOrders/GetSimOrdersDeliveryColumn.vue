5a10d24fc28f03ca40eb75ec196e5f13cf11ae75
Marwane <b.marwane@gmail.com>
2019-02-25 15:01:38 +0100

    simple check

--- src/views/GetSim/GetSimOrders/GetSimOrdersDeliveryColumn.vue
+++ src/views/GetSim/GetSimOrders/GetSimOrdersDeliveryColumn.vue
@@ -1,15 +1,16 @@
 <template>
-  <a :href="`mailto:${row.contactInformation.email}`">
-    {{ row.name.firstName }} {{ row.name.lastName }}
-  </a>
+  <div>
+    <a v-if="row.contactInformation && row.name" :href="`mailto:${row.contactInformation.email}`">
+      {{ row.name.firstName }} {{ row.name.lastName }}
+    </a>
+  </div>
+
 </template>
 
 <script>
 export default {
   props: {
-    row: {
-      type: Object,
-    },
+    row: Object,
   },
 };
 </script>
