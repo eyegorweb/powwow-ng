6a4e074717c7cf9347dc24b48851cc12156f6510
Marwane <b.marwane@gmail.com>
2019-03-05 09:51:11 +0100

    Scroll for panel

--- src/views/GetSim/GetSimOrderDetails.vue
+++ src/views/GetSim/GetSimOrderDetails.vue
@@ -1,163 +1,168 @@
 <template>
-  <div class="data-detail-container">
-    <div v-if="order.isNew" class="row shadow alert alert-success p-3 m-3" role="alert">
-      <div class="circle-loader load-complete">
-        <div class="checked checkmark" />
+  <div class="panel-vertical-container">
+    <div class="main-content">
+      <div v-if="order.isNew" class="row shadow alert alert-success p-3 m-3" role="alert">
+        <div class="circle-loader load-complete">
+          <div class="checked checkmark" />
+        </div>
+        <div class="ml-3">
+          {{ $t(('orders.new.message.confirmation')) }}<br>
+          {{ $t(('orders.new.message.delivery')) }}
+        </div>
+        <div class="cross-loader load-complete">
+          <a href="#" class="cross" @click.prevent="close" />
+        </div>
       </div>
-      <div class="ml-3">
-        {{ $t(('orders.new.message.confirmation')) }}<br>
-        {{ $t(('orders.new.message.delivery')) }}
+      <div class="overview-container m-3 bg-white">
+        <div class="overview-item">
+          <h4 class="font-weight-normal text-uppercase">{{ $t('orders.detail.information') }}</h4>
+        </div>
+        <div class="overview-item">
+          <StepperNonLinear v-if="!isCanceled" :stepper-data="steps" :current-index="steps.currentIndex" />
+          <StepperNonLinear v-if="isCanceled" :stepper-data="steps" />
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.detail.orderId') }} :</h6>
+          <p>#{{ getFromOrder('id') }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.detail.referenceClient') }} :</h6>
+          <p>{{ getFromOrder('externalId') }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.detail.creationDateOrder') }} :</h6>
+          <p>{{ getFromOrder('creationDate') }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.detail.creator') }} :</h6>
+          <p
+            class="m-0"
+          >{{ `${getFromOrder('auditable.creator.name.title')} ${getFromOrder('auditable.creator.name.firstName')} ${getFromOrder('auditable.creator.name.lastName')}` }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.detail.creatorMail') }} :</h6>
+          <p>{{ getFromOrder('contactInformation.email') }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.detail.manageID') }} :</h6>
+          <p>{{ getFromOrder('massActionIds') }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.detail.deliveryNumber') }} :</h6>
+          <p>{{ getFromOrder('deliveryNum') }}</p>
+        </div>
       </div>
-      <div class="cross-loader load-complete">
-        <a href="#" class="cross" @click.prevent="close" />
-      </div>
-    </div>
-    <div class="overview-container m-3 bg-white">
-      <div class="overview-item">
-        <h4 class="font-weight-normal text-uppercase">{{ $t('orders.detail.information') }}</h4>
-      </div>
-      <div class="overview-item">
-        <StepperNonLinear v-if="!isCanceled" :stepper-data="steps" :current-index="steps.currentIndex" />
-        <StepperNonLinear v-if="isCanceled" :stepper-data="steps" />
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.detail.orderId') }} :</h6>
-        <p>#{{ getFromOrder('id') }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.detail.referenceClient') }} :</h6>
-        <p>{{ getFromOrder('externalId') }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.detail.creationDateOrder') }} :</h6>
-        <p>{{ getFromOrder('creationDate') }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.detail.creator') }} :</h6>
-        <p
-          class="m-0"
-        >{{ `${getFromOrder('auditable.creator.name.title')} ${getFromOrder('auditable.creator.name.firstName')} ${getFromOrder('auditable.creator.name.lastName')}` }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.detail.creatorMail') }} :</h6>
-        <p>{{ getFromOrder('contactInformation.email') }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.detail.manageID') }} :</h6>
-        <p>{{ getFromOrder('massActionIds') }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.detail.deliveryNumber') }} :</h6>
-        <p>{{ getFromOrder('deliveryNum') }}</p>
-      </div>
-    </div>
 
-    <div class="overview-container m-3 bg-white">
-      <div class="overview-item">
-        <h4 class="font-weight-normal text-uppercase">{{ $t('orders.detail.simCardOffer') }}</h4>
+      <div class="overview-container m-3 bg-white">
+        <div class="overview-item">
+          <h4 class="font-weight-normal text-uppercase">{{ $t('orders.detail.simCardOffer') }}</h4>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('type') }} :</h6>
+          <p>{{ getFromOrder('orderedSIMCard.description') }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.detail.orderedQuantity') }} :</h6>
+          <p>{{ getFromOrder('quantity') }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.detail.importedQuantity') }} :</h6>
+          <p>{{ getFromOrder('importedQuantity') }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('action') }} :</h6>
+          <p
+            v-if="order.preActivationAsked && order.activationAsked"
+          >{{ $t('col.preActivationAsked') }} {{ $t('col.activationAsked') }}</p>
+          <p
+            v-if="order.preActivationAsked && !order.activationAsked"
+          >{{ $t('col.preActivationAsked') }}</p>
+          <p v-if="!order.preActivationAsked && order.activationAsked">{{ $t('col.activationAsked') }}</p>
+          <p v-if="!order.preActivationAsked && !order.activationAsked">{{ $t('none') }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('col.offer') }} :</h6>
+          <p>{{ getFromOrder('singleProduct.description') }}</p>
+        </div>
       </div>
-      <div class="overview-item">
-        <h6>{{ $t('type') }} :</h6>
-        <p>{{ getFromOrder('orderedSIMCard.description') }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.detail.orderedQuantity') }} :</h6>
-        <p>{{ getFromOrder('quantity') }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.detail.importedQuantity') }} :</h6>
-        <p>{{ getFromOrder('importedQuantity') }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('action') }} :</h6>
-        <p
-          v-if="order.preActivationAsked && order.activationAsked"
-        >{{ $t('col.preActivationAsked') }} {{ $t('col.activationAsked') }}</p>
-        <p
-          v-if="order.preActivationAsked && !order.activationAsked"
-        >{{ $t('col.preActivationAsked') }}</p>
-        <p v-if="!order.preActivationAsked && order.activationAsked">{{ $t('col.activationAsked') }}</p>
-        <p v-if="!order.preActivationAsked && !order.activationAsked">{{ $t('none') }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('col.offer') }} :</h6>
-        <p>{{ getFromOrder('singleProduct.description') }}</p>
-      </div>
-    </div>
 
-    <div class="overview-container m-3 bg-white">
-      <div class="overview-item">
-        <h4
-          class="font-weight-normal text-uppercase"
-        >{{ $t('orders.new.deliveryStep.form.deliveryCoords') }}</h4>
+      <div class="overview-container m-3 bg-white">
+        <div class="overview-item">
+          <h4
+            class="font-weight-normal text-uppercase"
+          >{{ $t('orders.new.deliveryStep.form.deliveryCoords') }}</h4>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.new.deliveryStep.form.company') }}</h6>
+          <p>{{ getFromOrder('company') }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('col.recipient') }}</h6>
+          <p
+            class="m-0"
+          >{{ `${getFromOrder('name.title')} ${getFromOrder('name.firstName')} ${getFromOrder('name.lastName')}` }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.new.deliveryStep.form.email') }}</h6>
+          <p>{{ getFromOrder('contactInformation.email') }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.new.deliveryStep.form.phone') }}</h6>
+          <p>{{ getFromOrder('contactInformation.phone') }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.new.deliveryStep.form.deliveryAddress') }}</h6>
+          <p>{{ getFromOrder('address') }}</p>
+        </div>
       </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.new.deliveryStep.form.company') }}</h6>
-        <p>{{ getFromOrder('company') }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('col.recipient') }}</h6>
-        <p
-          class="m-0"
-        >{{ `${getFromOrder('name.title')} ${getFromOrder('name.firstName')} ${getFromOrder('name.lastName')}` }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.new.deliveryStep.form.email') }}</h6>
-        <p>{{ getFromOrder('contactInformation.email') }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.new.deliveryStep.form.phone') }}</h6>
-        <p>{{ getFromOrder('contactInformation.phone') }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.new.deliveryStep.form.deliveryAddress') }}</h6>
-        <p>{{ getFromOrder('address') }}</p>
-      </div>
-    </div>
 
-    <div class="overview-container m-3 bg-white">
-      <div class="overview-item">
-        <h4 class="font-weight-normal text-uppercase">{{ $t('col.partner') }}</h4>
+      <div class="overview-container m-3 bg-white">
+        <div class="overview-item">
+          <h4 class="font-weight-normal text-uppercase">{{ $t('col.partner') }}</h4>
+        </div>
+        <div class="overview-item">
+          <h6>Code :</h6>
+          <p>{{ getFromOrder('party.code') }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.new.deliveryStep.form.firstname') }}</h6>
+          <p>{{ getFromOrder('party.name') }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.detail.contractReference') }} :</h6>
+          <p>{{ getFromOrder('party.contractReference') }}</p>
+        </div>
       </div>
-      <div class="overview-item">
-        <h6>Code :</h6>
-        <p>{{ getFromOrder('party.code') }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.new.deliveryStep.form.firstname') }}</h6>
-        <p>{{ getFromOrder('party.name') }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.detail.contractReference') }} :</h6>
-        <p>{{ getFromOrder('party.contractReference') }}</p>
-      </div>
-    </div>
 
-    <div class="overview-container m-3 bg-white">
-      <div class="overview-item">
-        <h4 class="font-weight-normal text-uppercase">{{ $t('common.billingAccount') }}</h4>
+      <div class="overview-container m-3 bg-white">
+        <div class="overview-item">
+          <h4 class="font-weight-normal text-uppercase">{{ $t('common.billingAccount') }}</h4>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('common.code') }} :</h6>
+          <p>{{ getFromOrder('customerAccount.code') }}</p>
+        </div>
+        <div class="overview-item">
+          <h6>{{ $t('orders.new.deliveryStep.form.address') }}</h6>
+          <p>{{ getFromOrder('customerAccount.address') }}</p>
+        </div>
       </div>
-      <div class="overview-item">
-        <h6>{{ $t('common.code') }} :</h6>
-        <p>{{ getFromOrder('customerAccount.code') }}</p>
-      </div>
-      <div class="overview-item">
-        <h6>{{ $t('orders.new.deliveryStep.form.address') }}</h6>
-        <p>{{ getFromOrder('customerAccount.address') }}</p>
-      </div>
-    </div>
 
-    <div class="overview-container m-3 bg-white">
-      <div class="overview-item">
-        <h4 class="font-weight-normal text-uppercase">{{ $t('orders.new.settings') }}</h4>
+      <div class="overview-container m-3 bg-white">
+        <div class="overview-item">
+          <h4 class="font-weight-normal text-uppercase">{{ $t('orders.new.settings') }}</h4>
+        </div>
+        <div v-for="field in customFields" :key="field.index" class="overview-item">
+          <h6>{{ field.label }} :</h6>
+          <p>{{ field.value }}</p>
+        </div>
       </div>
-      <div v-for="field in customFields" :key="field.index" class="overview-item">
-        <h6>{{ field.label }} :</h6>
-        <p>{{ field.value }}</p>
-      </div>
-    </div>
 
-    <UiButton variant="accent" block>{{ $t('orders.validate-order') }}</UiButton>
+    </div>
+    <div class="footer-bock">
+      <UiButton variant="accent" block>{{ $t('orders.validate-order') }}</UiButton>
+
+    </div>
   </div>
 </template>
 
@@ -319,9 +324,6 @@ export default {
 <style scoped lang="scss">
 $fontSize: 0.8rem;
 .data-detail-container {
-  overflow-y: auto;
-  height: 100%;
-
   $loader-size: 1.5em;
   $check-height: $loader-size/2;
   $check-width: $check-height/2;
