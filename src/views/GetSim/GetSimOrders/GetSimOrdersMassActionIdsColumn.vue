825bd73af49b254620135a6b643a6d6a99ce5fe3
Olivier Rabot <olivier.rabot3@docapost.fr>
2019-03-13 14:44:35 +0100

    Feat/261/id actes gestion

---
+++ src/views/GetSim/GetSimOrders/GetSimOrdersMassActionIdsColumn.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="massactions-ids d-flex flex-wrap justify-content-start align-items-center">
+      <p class="ids">
+        <span v-for="(id, index) in actionIds" :key="id" class="id">{{ index ? ', ' : '' }}{{ id }}</span>
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    row: Object,
+  },
+  computed: {
+    actionIds() {
+      return this.row.massActionIds;
+    },
+  },
+};
+</script>
