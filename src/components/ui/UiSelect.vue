41f88d547b7e370e013bc775560ef6a3c22fda25
Marwane <b.marwane@gmail.com>
2019-03-19 10:55:38 +0100

    Fix UiSelect

--- src/components/ui/UiSelect.vue
+++ src/components/ui/UiSelect.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="select-container">
-    <select
-      class="form-control"
-      v-model="model"
-      :class="{ 'arrow-blue': arrowBlue, error: !!error }"
-      :size="numberOfVisibleItems"
-    >
-      <slot>
-        <option v-if="placeholder" disabled selected :value="null">{{ placeholder }}</option>
-        <option v-for="option in formattedOptions" :key="option.value" :value="option.value">{{
-          option.label
-        }}</option>
-      </slot>
-    </select>
+    <div class="select">
+      <select
+        class="form-control"
+        v-model="model"
+        :class="{ 'arrow-blue': arrowBlue, error: !!error }"
+        :size="numberOfVisibleItems"
+      >
+        <slot>
+          <option v-if="placeholder" disabled selected :value="null">{{ placeholder }}</option>
+          <option v-for="option in formattedOptions" :key="option.value" :value="option.value">{{
+            option.label
+          }}</option>
+        </slot>
+      </select>
+    </div>
     <span v-if="error" class="error-text">{{ $t(error) }}</span>
   </div>
 </template>
@@ -64,42 +66,14 @@ export default {
   border: 1px solid $orange;
   border-radius: 3px;
 }
-.form-control {
-  display: inline-block;
-  font-family: 'Open Sans', sans-serif;
-  font-size: 0.9rem;
-  border-radius: $box-radius;
-  width: 100%;
-}
 
 .select-container {
   display: inline-block;
 }
 
 select {
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  color: inherit;
-  font-size: 16px;
-  border: 1px solid $medium-gray;
-  width: auto;
   padding: 0.5rem 0.8rem;
-  padding-right: 3rem;
-  /* TODO:Utiliser la bonne icone */
-  background-image: url('../../assets/arrow.svg');
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-position: right center;
-
-  &:disabled {
-    background-color: $light-gray;
-  }
-}
-
-.arrow-blue {
-  background-image: url('../../assets/arrow-blue.svg');
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-position: right center;
+  padding-right: 0 !important;
+  border-radius: 0;
 }
 </style>
