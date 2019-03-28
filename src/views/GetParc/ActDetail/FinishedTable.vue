5aa8fa6ee4b509ce1a7f65f6a1a061d1140e0a5a
Marwane Belahcen <marwane.belahcen3@docapost.fr>
2019-03-28 15:49:04 +0100

    Feat/264/detail history

---
+++ src/views/GetParc/ActDetail/FinishedTable.vue
@@ -0,0 +1,138 @@
+<template>
+  <LoaderContainer :is-loading="false">
+    <div>
+      <div class="row mb-3">
+        <div class="col">
+          <h2 class="text-gray font-weight-light" style="font-size: 2rem">
+            {{ $t('getparc.actDetail.title', { total: total }) }}
+          </h2>
+        </div>
+      </div>
+      <DataTable
+        :columns.sync="columns"
+        :rows="rows || []"
+        :page.sync="page"
+        :page-limit.sync="pageLimit"
+        :total="total || 0"
+        :order-by.sync="orderBy"
+        :show-extra-columns.sync="showExtraCells"
+        @change-order="changeCellsOrder"
+        :size="7"
+      >
+        <template slot="topLeftCorner">
+          search input
+        </template>
+      </DataTable>
+    </div>
+  </LoaderContainer>
+</template>
+
+<script>
+import DataTable from '@/components/DataTable/DataTable';
+import LoaderContainer from '@/components/LoaderContainer';
+
+export default {
+  components: {
+    DataTable,
+    LoaderContainer,
+  },
+  props: {
+    rows: {
+      type: Array,
+    },
+    total: Number,
+  },
+  data() {
+    return {
+      columns: [
+        {
+          id: 1,
+          label: this.$t('getparc.actDetail.col.id'),
+          name: 'id',
+          orderable: true,
+          visible: true,
+        },
+
+        {
+          id: 2,
+          label: this.$t('getparc.actDetail.col.msisdn'),
+          name: 'msisdn',
+          orderable: true,
+          visible: true,
+        },
+        {
+          id: 3,
+          label: this.$t('getparc.actDetail.col.iccid'),
+          name: 'iccid',
+          orderable: true,
+          visible: true,
+        },
+        {
+          id: 4,
+          label: this.$t('getparc.actDetail.col.actState'),
+          name: 'actState',
+          orderable: true,
+          visible: true,
+        },
+        {
+          id: 6,
+          label: this.$t('getparc.actDetail.col.startDate'),
+          name: 'startDate',
+          orderable: true,
+          visible: true,
+        },
+        {
+          id: 7,
+          label: this.$t('getparc.actDetail.col.endDate'),
+          name: 'endDate',
+          orderable: true,
+          visible: true,
+        },
+        {
+          id: 8,
+          label: this.$t('getparc.actDetail.col.imsi'),
+          name: 'imsi',
+          orderable: true,
+          visible: true,
+        },
+        {
+          id: 10,
+          label: this.$t('getparc.actDetail.col.constructor'),
+          name: 'constructor',
+          orderable: true,
+          visible: false,
+        },
+        {
+          id: 11,
+          label: this.$t('getparc.actDetail.col.commercialRef'),
+          name: 'commercialRef',
+          orderable: true,
+          visible: false,
+        },
+        {
+          id: 12,
+          label: this.$t('getparc.actDetail.col.imei'),
+          name: 'imei',
+          orderable: true,
+          visible: false,
+        },
+      ],
+      page: 0,
+      pageLimit: 20,
+      orderBy: {
+        key: 'id',
+        direction: 'DESC',
+      },
+      showExtraCells: false,
+    };
+  },
+  methods: {
+    changeCellsOrder(orderedCells) {
+      const notVisibleCells = this.columns.filter(c => !c.visible);
+      this.columns = orderedCells.concat(notVisibleCells);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/GetParc/ActDetail/index.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="mt-4">
+    <UiTabs :tabs="tabs" :selected-index="currentLinkIndex">
+      <template slot-scope="{ tab, index, selectedIndex }">
+        <UiTab v-if="tab" :is-selected="index === selectedIndex" class="tab-grow">
+          <a href="#" @click.prevent="() => (currentLinkIndex = index)">
+            {{ tab.title }}
+            <span
+              class="badge badge-pill"
+              :class="{
+                'badge-danger': index === 0,
+                'badge-info': index === 1,
+                'badge-success': index === 2,
+              }"
+            >
+              {{ tab.total }}
+            </span>
+          </a>
+        </UiTab>
+      </template>
+      <div slot="fail">
+        <FailedTable :rows="tabs[0].rows" :total="tabs[0].total" />
+      </div>
+      <div slot="ongoing">
+        <OngoingTable :rows="tabs[1].rows" :total="tabs[1].total" />
+      </div>
+      <div slot="finished">
+        <FinishedTable :rows="tabs[2].rows" :total="tabs[2].total" />
+      </div>
+    </UiTabs>
+  </div>
+</template>
+
+<script>
+import UiTabs from '@/components/ui/Tabs';
+import UiTab from '@/components/ui/Tab';
+import FailedTable from './FailedTable';
+import OngoingTable from './OngoingTable';
+import FinishedTable from './FinishedTable';
+
+export default {
+  components: {
+    UiTabs,
+    UiTab,
+    FailedTable,
+    OngoingTable,
+    FinishedTable,
+  },
+  data() {
+    return {
+      currentLinkIndex: 0,
+      tabs: [
+        {
+          label: 'fail',
+          title: 'LISTE DES CARTES EN ÉCHEC',
+          total: 1,
+          rows: [
+            {
+              id: '567990324433324',
+              msisdn: '33761456934',
+              iccid: '8933200809935003869',
+              actState: 'Ancienne offre: ENEDYS-LINKY-PROD',
+              failDate: '18/11/2018 14:56:09',
+              failReason: `Le changement d'offre est invalide`,
+              imsi: '0123',
+              constructor: 'NOKIA',
+              commercialRef: 'ref001',
+              imei: '1234',
+            },
+          ],
+        },
+        {
+          label: 'ongoing',
+          title: 'LISTE DES CARTES EN COURS',
+          total: 1,
+          rows: [
+            {
+              id: '567990324433324',
+              msisdn: '33761456934',
+              iccid: '8933200809935003869',
+              actState: 'Ancienne offre: ENEDYS-LINKY-PROD',
+              startDate: '18/11/2018 14:56:09',
+              imsi: '0123',
+              constructor: 'NOKIA',
+              commercialRef: 'ref001',
+              imei: '1234',
+            },
+          ],
+        },
+        {
+          label: 'finished',
+          title: 'LISTE DES CARTES TERMINÉES',
+          total: 1,
+          rows: [
+            {
+              id: '567990324433324',
+              msisdn: '33761456934',
+              iccid: '8933200809935003869',
+              actState: 'Ancienne offre: ENEDYS-LINKY-PROD',
+              startDate: '18/11/2018 14:56:09',
+              endDate: '20/11/2018 16:56:09',
+              imsi: '0123',
+              constructor: 'NOKIA',
+              commercialRef: 'ref001',
+              imei: '1234',
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.tab-grow {
+  flex-grow: 1;
+}
+</style>
