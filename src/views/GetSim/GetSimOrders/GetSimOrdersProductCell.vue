9c0c8858807b3c2b3b5d244065a25a266502d407
Olivier Rabot <olivier.rabot3@docapost.fr>
2019-04-19 12:48:15 +0000

    Feat/306/composant donnees tronquees

---
+++ src/views/GetSim/GetSimOrders/GetSimOrdersProductCell.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="truncate">
+    <Truncate :limit="2" :content="row.orderedSimcard.description"></Truncate>
+  </div>
+</template>
+
+<script>
+import Truncate from '@/components/ui/Truncate';
+
+export default {
+  components: {
+    Truncate,
+  },
+  props: {
+    row: Object,
+  },
+};
+</script>
+
+<style scoped>
+.truncate {
+  max-width: 10rem;
+}
+</style>
