2bad4e87b68a35473ff6b31c62b5bacdc833ce16
Igor Trifunovic <igor.trifunovic3@docapost.fr>
2019-04-24 13:03:12 +0000

    fix height and scroll

--- src/views/GetSim/CreateOrder/StepSettings/CreateOrderAddCustomField.vue
+++ src/views/GetSim/CreateOrder/StepSettings/CreateOrderAddCustomField.vue
@@ -1,126 +1,129 @@
 <template>
-  <div class="custom-field-form-container">
-    <h2 class="title mb-2">{{ $t('orders.add-custom-field') }}</h2>
+  <div class="main-content">
+    <h3 class="font-weight-light text-center mt-4 mb-4">{{ $t('orders.add-custom-field') }}</h3>
     <p class="subtitle">{{ $t('orders.required-custom-field') }}</p>
-    <div class="subcontainer">
-      <UiInput class="d-block" placeholder v-model="labelCustomField" value>
-        <template slot="beforeInput"
-          >{{ $t('orders.input-label-custom-field') }} :</template
-        >
-      </UiInput>
-      <div>
-        <label class="standalone">{{ $t('orders.choose-type-custom-field') }} :</label>
-        <!-- TODO extraire le btn-group dans un composant avec des slots -->
-        <div class="buttons-group-container mt-1">
-          <div class="btn-group w-100" role="group">
-            <button
-              type="button"
-              class="btn float-left"
-              :class="{
-                'btn-primary': customFieldType === 'TEXT',
-                ' btn-outline-simple': customFieldType !== 'TEXT',
-              }"
-              @click="customFieldType = 'TEXT'"
-            >
-              {{ $t('text') }}
-            </button>
-            <button
-              type="button"
-              class="btn float-left"
-              :class="{
-                'btn-primary': customFieldType === 'LIST',
-                ' btn-outline-simple': customFieldType !== 'LIST',
-              }"
-              @click="customFieldType = 'LIST'"
-            >
-              {{ $t('list') }}
-            </button>
-            <button
-              type="button"
-              class="btn float-left"
-              :class="{
-                'btn-primary': customFieldType === 'DATE',
-                ' btn-outline-simple': customFieldType !== 'DATE',
-              }"
-              @click="customFieldType = 'DATE'"
-            >
-              {{ $t('date') }}
-            </button>
-          </div>
-        </div>
-        <div>
-          <div v-if="customFieldType === 'LIST'">
-            <CreateOrderAddCustomList :options.sync="listOptions" />
-          </div>
-        </div>
-      </div>
-
-      <div>
-        <label class="standalone mb-2">{{ $t('orders.new.settingsStep.mandatoryField') }} :</label>
-
-        <div class="row mb-2">
-          <div class="col">
-            <UiCheckbox
-              class="float-left"
-              input-type="radio"
-              shape="round"
-              name="orderRequired"
-              v-model="selectedMandatoryValue"
-              value="NONE"
-            />
-            <div class="float-left">{{ $t('orders.new.settingsStep.NONE') }}</div>
-          </div>
-
-          <div class="col">
-            <UiCheckbox
-              class="float-left"
-              input-type="radio"
-              shape="round"
-              name="orderRequired"
-              v-model="selectedMandatoryValue"
-              value="ORDER"
-            />
-            <div class="float-left">{{ $t('orders.new.settingsStep.ORDER') }}</div>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col">
-            <UiCheckbox
-              class="float-left"
-              input-type="radio"
-              shape="round"
-              name="orderRequired"
-              v-model="selectedMandatoryValue"
-              value="ACTIVATION"
-            />
-            <div class="float-left">{{ $t('orders.new.settingsStep.ACTIVATION') }}</div>
-          </div>
-
-          <div class="col">
-            <UiCheckbox
-              class="float-left"
-              input-type="radio"
-              shape="round"
-              name="orderRequired"
-              v-model="selectedMandatoryValue"
-              value="PAIRING"
-            />
-            <div class="float-left">{{ $t('orders.new.settingsStep.PAIRING') }}</div>
-          </div>
-        </div>
-      </div>
-
-      <UiButton @click="$emit('cancel')" variant="outline-primary" class="float-left">{{
-        $t('cancel')
-      }}</UiButton>
-      <UiButton
-        :variant="canAddCustomField() ? 'primary' : 'outline-simple'"
-        class="float-right"
-        :disabled="!canAddCustomField()"
-        @click="saveCustomField"
-        >{{ $t('orders.add-custom-field') }}</UiButton
+    <UiInput class="d-block" placeholder v-model="labelCustomField" value>
+      <template slot="beforeInput"
+        >{{ $t('orders.input-label-custom-field') }} :</template
       >
+    </UiInput>
+    <div>
+      <label class="standalone">{{ $t('orders.choose-type-custom-field') }} :</label>
+      <!-- TODO extraire le btn-group dans un composant avec des slots -->
+      <div class="buttons-group-container mt-1">
+        <div class="btn-group w-100" role="group">
+          <button
+            type="button"
+            class="btn float-left"
+            :class="{
+              'btn-primary': customFieldType === 'TEXT',
+              ' btn-outline-simple': customFieldType !== 'TEXT',
+            }"
+            @click="customFieldType = 'TEXT'"
+          >
+            {{ $t('text') }}
+          </button>
+          <button
+            type="button"
+            class="btn float-left"
+            :class="{
+              'btn-primary': customFieldType === 'LIST',
+              ' btn-outline-simple': customFieldType !== 'LIST',
+            }"
+            @click="customFieldType = 'LIST'"
+          >
+            {{ $t('list') }}
+          </button>
+          <button
+            type="button"
+            class="btn float-left"
+            :class="{
+              'btn-primary': customFieldType === 'DATE',
+              ' btn-outline-simple': customFieldType !== 'DATE',
+            }"
+            @click="customFieldType = 'DATE'"
+          >
+            {{ $t('date') }}
+          </button>
+        </div>
+      </div>
+      <div>
+        <div v-if="customFieldType === 'LIST'">
+          <CreateOrderAddCustomList :options.sync="listOptions" />
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <label class="standalone mb-2">{{ $t('orders.new.settingsStep.mandatoryField') }} :</label>
+
+      <div class="row mb-2">
+        <div class="col">
+          <UiCheckbox
+            class="float-left"
+            input-type="radio"
+            shape="round"
+            name="orderRequired"
+            v-model="selectedMandatoryValue"
+            value="NONE"
+          />
+          <div class="float-left">{{ $t('orders.new.settingsStep.NONE') }}</div>
+        </div>
+
+        <div class="col">
+          <UiCheckbox
+            class="float-left"
+            input-type="radio"
+            shape="round"
+            name="orderRequired"
+            v-model="selectedMandatoryValue"
+            value="ORDER"
+          />
+          <div class="float-left">{{ $t('orders.new.settingsStep.ORDER') }}</div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <UiCheckbox
+            class="float-left"
+            input-type="radio"
+            shape="round"
+            name="orderRequired"
+            v-model="selectedMandatoryValue"
+            value="ACTIVATION"
+          />
+          <div class="float-left">{{ $t('orders.new.settingsStep.ACTIVATION') }}</div>
+        </div>
+
+        <div class="col">
+          <UiCheckbox
+            class="float-left"
+            input-type="radio"
+            shape="round"
+            name="orderRequired"
+            v-model="selectedMandatoryValue"
+            value="PAIRING"
+          />
+          <div class="float-left">{{ $t('orders.new.settingsStep.PAIRING') }}</div>
+        </div>
+      </div>
+    </div>
+    <div class="row mt-4">
+      <div class="col">
+        <UiButton @click="$emit('cancel')" variant="outline-primary" class="float-left">{{
+        $t('cancel')
+        }}</UiButton>
+      </div>
+      <div class="col">
+        <UiButton
+          :variant="canAddCustomField() ? 'primary' : 'outline-simple'"
+          class="float-right"
+          :disabled="!canAddCustomField()"
+          @click="saveCustomField"
+          >{{ $t('orders.add-custom-field') }}</UiButton
+        >
+      </div>
     </div>
   </div>
 </template>
@@ -194,13 +197,7 @@ export default {
   padding: 0;
   text-align: center;
 }
-.subcontainer {
-  width: 65%;
-  margin: 0 auto;
-  > div {
-    margin-bottom: 2rem;
-  }
-}
+
 label {
   font-weight: 600;
   font-size: 0.875rem;
@@ -237,11 +234,6 @@ label {
   .tabs-bar::after {
     display: none !important;
   }
-}
-
-.custom-field-form-container {
-  max-height: 46rem;
-  overflow-y: auto;
 }
 
 .buttons-group-container {
