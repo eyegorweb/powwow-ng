16951f7a28edf960fff8cc33a35f2fc337973378
Richard Hallier <richard.hallier3@docapost.fr>
2019-04-26 12:14:14 +0000

    For demo purpose, hide the bytel logo

--- src/components/NavBars/NavBarBackoffice.vue
+++ src/components/NavBars/NavBarBackoffice.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg">
-    <div class="logoPart">
+    <div class="logoPart" style="display: none">
       <img class="logo" src="@/assets/logo_bouygues.png" alt />
     </div>
     <div class="contentPart">
