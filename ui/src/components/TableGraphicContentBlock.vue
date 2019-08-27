002ca8dc9d3f6f307ca31dacccfc50725f95ce7d
Marwane <b.marwane@gmail.com>
2019-08-27 11:34:23 +0100

    Composant toggle graphique/Table

---
+++ ui/src/components/TableGraphicContentBlock.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="d-flex justify-content-end align-items-center">
+      {{ $t('display') }}:
+      <Toggle @update="updateContentType" :values="toggleValues" class="pl-2"></Toggle>
+    </div>
+    <div>
+      <slot v-if="contentType === 'graph'" name="graph">Graph</slot>
+      <slot v-if="contentType === 'table'" name="table">Table</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import Toggle from '@/components/ui/UiToggle2';
+
+export default {
+  components: {
+    Toggle,
+  },
+  data() {
+    return {
+      contentType: 'table',
+      toggleValues: [
+        {
+          id: 'graph',
+          label: 'getparc.lineDetail.tab2.lineAnalysisContent.toggle.graph',
+        },
+        {
+          id: 'table',
+          label: 'getparc.lineDetail.tab2.lineAnalysisContent.toggle.table',
+          default: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    updateContentType(newVal) {
+      this.contentType = newVal.id;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- ui/src/views/GetParc/LineDetail/DiagnosisTab/Supervision/index.vue
+++ ui/src/views/GetParc/LineDetail/DiagnosisTab/Supervision/index.vue
@@ -3,14 +3,14 @@
     <draggable handle=".handle">
       <transition-group>
         <ContentBlock :key="'block1'">
-          <template slot="title">{{
-            $t('getparc.lineDetail.tab2.supervisionContent.dataConsumptionPerDay')
-          }}</template>
+          <template slot="title">
+            {{ $t('getparc.lineDetail.tab2.supervisionContent.dataConsumptionPerDay') }}
+          </template>
           <template slot="content">
-            <div class="d-flex justify-content-end align-items-center">
-              {{ $t('display') }}:
-              <Toggle @update="updateContentType" :values="toggleValues" class="pl-2"></Toggle>
-            </div>
+            <TableGraphicContentBlock>
+              <div slot="graph">Graphic here</div>
+              <div slot="table">Table here</div>
+            </TableGraphicContentBlock>
           </template>
         </ContentBlock>
       </transition-group>
@@ -20,38 +20,18 @@
 
 <script>
 import ContentBlock from '@/views/GetParc/LineDetail/ContentBlock';
+import TableGraphicContentBlock from '@/components/TableGraphicContentBlock';
 import draggable from 'vuedraggable';
-import Toggle from '@/components/ui/UiToggle2';
 
 export default {
   components: {
     draggable,
     ContentBlock,
-    Toggle,
+    TableGraphicContentBlock,
   },
-  data() {
-    return {
-      contentType: 'table',
-      toggleValues: [
-        {
-          id: 'graph',
-          label: 'getparc.lineDetail.tab2.lineAnalysisContent.toggle.graph',
-        },
-        {
-          id: 'table',
-          label: 'getparc.lineDetail.tab2.lineAnalysisContent.toggle.table',
-          default: true,
-        },
-      ],
-    };
-  },
+
   props: {
     content: Object,
-  },
-  methods: {
-    updateContentType(newVal) {
-      this.contentType = newVal.id;
-    },
   },
 };
 </script>
