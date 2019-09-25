5a4fd6d90afdd3169d92aeea577db7accb5c5255
Marwane <b.marwane@gmail.com>
2019-09-25 17:29:07 +0100

    fix label

--- ui/src/views/GetParc/LineDetail/DiagnosisTab/NetworkTestControl/index.vue
+++ ui/src/views/GetParc/LineDetail/DiagnosisTab/NetworkTestControl/index.vue
@@ -11,7 +11,7 @@
       <div class="row">
         <div class="col-md-12">
           <button @click="openNewDemandModal" class="btn btn-xs float-right btn-primary">
-            {{ $t('getparc.actDetail.discharge') }}
+            Ajouter une nouvelle demande
           </button>
         </div>
       </div>
