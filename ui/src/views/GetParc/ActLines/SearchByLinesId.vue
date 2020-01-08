b04346e9978004a65c319a103a14041aa0bdff3b
Marwane <b.marwane@gmail.com>
2020-01-08 10:05:13 +0100

    remettre la recherche par AP_ID

--- ui/src/views/GetParc/ActLines/SearchByLinesId.vue
+++ ui/src/views/GetParc/ActLines/SearchByLinesId.vue
@@ -1,9 +1,16 @@
 <template>
-  <SearchById @searchById="searchById" :init-value="initValue" placeholder="searchLine" />
+  <SearchById
+    v-if="additionalIds"
+    @searchById="searchById"
+    :init-value="initValue"
+    :additional-ids="additionalIds"
+    placeholder="searchLine"
+  />
 </template>
 
 <script>
 import SearchById from '@/components/SearchById';
+import { mapGetters } from 'vuex';
 
 export default {
   components: {
@@ -14,8 +21,34 @@ export default {
       type: String,
     },
   },
-
+  mounted() {
+    if (this.userIsBO) {
+      this.additionalIds = [
+        {
+          code: 'c6',
+          value: 'accessPointId',
+          label: 'AP_ID',
+          checkFn: value => {
+            if (isNaN(value)) return true;
+            return (
+              value.length !== 0 &&
+              (value.length !== 19 || value.length !== 15 || value.length !== 11)
+            );
+          },
+        },
+      ];
+    } else {
+      this.additionalIds = [];
+    }
+  },
+  data() {
+    return {
+      additionalIds: undefined,
+    };
+  },
   methods: {
+    ...mapGetters(['userIsBO']),
+
     searchById(params) {
       this.$emit('searchById', params);
     },
