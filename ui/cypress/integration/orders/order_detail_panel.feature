94dde91639a750cc7548bbc0ec5536183e8906ad
Marwane Belahcen <marwane.belahcen3@docapost.fr>
2020-03-03 13:48:02 +0000

    fix: corriger l'export d'actes

---
+++ ui/cypress/integration/orders/order_detail_panel.feature
@@ -0,0 +1,6 @@
+Feature: Paneaux de détail de commandes
+
+  Je veux voir les informations sur le détail de la commande
+
+  Scenario: le bouton "Importer des cartes SIM" doit apparaitre si la quantité importée est inférieure strictement à la quantité de la commande
+  
--- ui/src/views/GetVision/alarms/ParcAlarmsTab/AlarmsTable/AlarmsActions.vue
+++ ui/src/views/GetVision/alarms/ParcAlarmsTab/AlarmsTable/AlarmsActions.vue
@@ -4,6 +4,8 @@
 
 <script>
 import ActionButtons from '@/components/DataTable/ActionButtons.vue';
+import { enableAlarm, disableAlarm, deleteAlarm } from '@/api/alarms';
+import { mapMutations } from 'vuex';
 
 export default {
   components: {
@@ -12,16 +14,58 @@ export default {
   props: {
     alarm: Object,
   },
+
+  computed: {
+    actions() {
+      let additionalActions = [];
+
+      if (this.alarm.disabled) {
+        additionalActions.push('actions.DISABLE');
+      } else {
+        additionalActions.push('actions.ENABLE');
+      }
+
+      return ['getsim.actions.DETAIL', ...additionalActions];
+    },
+  },
+
   data() {
     return {
-      actions: ['getsim.actions.DETAIL'],
+      canDoAsyncAction: true,
     };
   },
 
   methods: {
-    onActionClicked(action) {
+    ...mapMutations(['flashMessage']),
+
+    async onActionClicked(action) {
       if (action === 'getsim.actions.DETAIL') {
         this.$router.push({ name: 'alarmDetail', params: { alarmId: this.alarm.id } });
+      }
+
+      if (!this.canDoAsyncAction) return;
+
+      let response;
+      this.canDoAsyncAction = false;
+
+      if (action === 'actions.ENABLE') {
+        response = await enableAlarm(this.alarm.id);
+      }
+
+      if (action === 'actions.DISABLE') {
+        response = await disableAlarm(this.alarm.id);
+      }
+
+      if (action === 'actions.DELETE') {
+        response = await deleteAlarm(this.alarm.id);
+      }
+
+      this.canDoAsyncAction = true;
+
+      if (response) {
+        this.flashMessage({ level: 'success', message: this.$t('genericSuccessMessage') });
+      } else {
+        this.flashMessage({ level: 'danger', message: this.$t('genericErrorMessage') });
       }
     },
   },
