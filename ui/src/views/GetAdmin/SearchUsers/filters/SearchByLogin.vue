db46e172964278b5e9b7a01c6a1b43321be1f276
Igor Trifunovic <igor.trifunovic3@docapost.fr>
2020-03-06 16:01:03 +0000

    feat: création de la page d'index Gestion des utilisateurs et ajout de filtres de recherche

---
+++ ui/src/views/GetAdmin/SearchUsers/filters/SearchByLogin.vue
@@ -0,0 +1,67 @@
+<template>
+  <form class="searchInput" @submit.prevent="searchByLogin">
+    <div class="form-row">
+      <div class="form-group mb-0 col-md-8">
+        <UiInput
+          :placeholder="$t('searchByLoginPlaceholder')"
+          class="d-block"
+          v-model="searchValue"
+          have-cross-button
+        >
+          <i slot="icon" class="select-icon ic-Magnify-Icon" />
+        </UiInput>
+      </div>
+      <div class="form-group col-md-3 mb-0">
+        <UiButton
+          :variant="isDisabled ? 'primary' : ''"
+          :disabled="!isDisabled"
+          @click="searchByLogin"
+          >{{ $t('search') }}</UiButton
+        >
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+import UiInput from '@/components/ui/UiInput';
+import UiButton from '@/components/ui/Button';
+
+export default {
+  components: {
+    UiInput,
+    UiButton,
+  },
+  props: {
+    initValue: String,
+  },
+  mounted() {
+    this.searchValue = this.initValue;
+  },
+  data() {
+    return {
+      searchValue: undefined,
+    };
+  },
+  methods: {
+    searchByLogin() {
+      this.$emit('searchByLogin', this.searchValue);
+    },
+  },
+  computed: {
+    isDisabled() {
+      return !!this.searchValue;
+    },
+  },
+
+  watch: {
+    searchValue(newValue, oldValue) {
+      if (oldValue && !newValue) {
+        this.$emit('inputIsEmpty');
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ ui/src/views/GetAdmin/SearchUsers/index.vue
@@ -0,0 +1,216 @@
+<template>
+  <div class="mt-4">
+    <div class="row mb-5">
+      <div class="col-md-9">
+        <h4>
+          <b>GetAdmin</b>
+          - {{ $t('getadmin.users.title') }}
+          <Tooltip direction="right">{{ $t('getvsion.manage-alarms-tooltip') }}</Tooltip>
+        </h4>
+      </div>
+    </div>
+    <TableWithFilter
+      :filters="filters"
+      :columns="columns"
+      :rows="rows"
+      :total="total"
+      :order-by.sync="orderBy"
+      :size="0"
+      @applyFilters="applyFilters"
+      @colEvent="onColEvent"
+    >
+      <div slot="title">{{ $t('getadmin.users.total', { total: total }) }}</div>
+
+      <div slot="topLeft">
+        <SearchByLogin
+          @searchByLogin="searchByLogin"
+          @inputIsEmpty="clearSearch"
+          :init-value="searchByLoginValue"
+        />
+      </div>
+    </TableWithFilter>
+  </div>
+</template>
+
+<script>
+import Tooltip from '@/components/ui/Tooltip';
+import TableWithFilter from '@/components/Filters/TableWithFilter';
+import FullNameFilter from './filters/FullNameFilter';
+import EmailFilter from './filters/EmailFilter';
+import SearchByLogin from './filters/SearchByLogin';
+import GroupPartnerFilter from './filters/GroupPartnerFilter';
+import { searchUsers } from '@/api/users';
+import get from 'lodash.get';
+
+export default {
+  components: {
+    Tooltip,
+    TableWithFilter,
+    SearchByLogin,
+  },
+  data() {
+    return {
+      filters: [
+        {
+          title: 'getadmin.users.fullName',
+          component: FullNameFilter,
+          onChange(chosenValue) {
+            return {
+              id: 'getadmin.users.fullName',
+              value: chosenValue,
+            };
+          },
+        },
+        {
+          title: 'getadmin.users.email',
+          component: EmailFilter,
+          onChange(chosenValue) {
+            return {
+              id: 'getadmin.users.email',
+              value: chosenValue,
+            };
+          },
+        },
+        {
+          title: 'getadmin.users.partnerGroup',
+          component: GroupPartnerFilter,
+          onChange(chosenValues) {
+            return {
+              id: 'getadmin.users.partnerGroup',
+              values: chosenValues,
+            };
+          },
+        },
+      ],
+      columns: [
+        {
+          id: 1,
+          label: 'ID',
+          name: 'id',
+          orderable: true,
+          visible: true,
+          noHandle: true,
+        },
+        {
+          id: 2,
+          label: 'Login',
+          name: 'username',
+          orderable: true,
+          visible: true,
+          noHandle: true,
+        },
+        {
+          id: 3,
+          label: 'Nom',
+          name: 'lastname',
+          orderable: true,
+          visible: true,
+          noHandle: true,
+          format: {
+            type: 'Getter',
+            getter: row => {
+              return get(row, 'name.lastName');
+            },
+          },
+        },
+        {
+          id: 4,
+          label: 'Prénom',
+          name: 'firstname',
+          orderable: true,
+          visible: true,
+          noHandle: true,
+          format: {
+            type: 'Getter',
+            getter: row => {
+              return get(row, 'name.firstName');
+            },
+          },
+        },
+        {
+          id: 4,
+          label: 'Partenaire',
+          name: 'partner',
+          orderable: true,
+          visible: true,
+          noHandle: true,
+          format: {
+            type: 'Getter',
+            getter: row => {
+              return get(row, 'party.name');
+            },
+          },
+        },
+        {
+          id: 5,
+          label: 'Rôles',
+          name: 'roles',
+          orderable: false,
+          visible: true,
+          noHandle: true,
+        },
+        {
+          id: 6,
+          label: 'Actif',
+          name: 'disabled',
+          orderable: true,
+          visible: true,
+          noHandle: true,
+          format: {
+            type: 'Getter',
+            getter: row => {
+              return row.disabled ? 'Inactif' : 'Actif';
+            },
+          },
+        },
+      ],
+      rows: [],
+      total: 0,
+      orderBy: {
+        key: 'id',
+        direction: 'DESC',
+      },
+      searchByLoginValue: undefined,
+      searchLoginResults: [],
+    };
+  },
+  mounted() {
+    this.applyFilters();
+  },
+  methods: {
+    async applyFilters(payload) {
+      const { pagination, filters } = payload || {
+        pagination: { page: 0, limit: 10 },
+        filters: [],
+      };
+
+      const data = await searchUsers(this.orderBy, pagination, filters);
+      this.total = data.total;
+      this.rows = data.items;
+    },
+    onColEvent(payload) {
+      console.log('col event', payload);
+    },
+    async searchByLogin(value) {
+      this.searchByLoginValue = value;
+      const data = await searchUsers(this.orderBy, { page: 0, limit: 10 }, [
+        {
+          id: 'getadmin.users.login',
+          value,
+        },
+      ]);
+      this.total = data.total;
+      this.rows = data.items;
+    },
+
+    clearSearch() {
+      if (this.searchByLoginValue) {
+        this.searchByLoginValue = undefined;
+        this.applyFilters();
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
