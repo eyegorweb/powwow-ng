4595adc86b11e15785af11fb8cd9025a99db1ee0
Igor Trifunovic <igor.trifunovic3@docapost.fr>
2020-04-01 15:00:33 +0000

    fix: correction pour assigner immédiatement les indicateurs des commandes

--- ui/src/views/GetSim/GetSimIndicators.vue
+++ ui/src/views/GetSim/GetSimIndicators.vue
@@ -1,6 +1,6 @@
 <template>
   <Indicators
-    :key="indicatorsVersion"
+    v-if="indicators"
     :meta="indicators"
     :on-click="indicator => $emit('click', indicator)"
     :no-borders="noBorders"
@@ -38,86 +38,43 @@ export default {
   },
   methods: {
     ...mapMutations('getsim', ['refreshIndicators']),
-  },
-  mounted() {
-    let indicators = [
-      {
-        name: 'toBeConfirmed',
-        labelKey: 'indicators.getsim.ordersToBeConfirmed',
-        color: 'text-danger',
-        clickable: true,
-        total: '-',
-        filters: [
-          {
-            id: 'filters.orderStatus',
-            values: [{ id: 'NOT_VALIDATED', label: this.$t('col.statuses.NOT_VALIDATED') }],
-          },
-          {
-            id: 'filters.orderDate',
-            startDate: moment()
-              .subtract(6, 'month')
-              .format(dateFormat),
-            endDate: moment().format(dateFormat),
-          },
-        ],
-        fetchKey: 'ORDER_TO_VALIDATE',
-        fetch: async (indicator, contextFilters) => {
-          return await fetchSingleIndicator(indicator.filters, contextFilters);
-        },
-      },
-      {
-        name: 'ordersInProgress',
-        labelKey: 'indicators.getsim.ordersInProgress',
-        color: 'text-success',
-        clickable: true,
-        total: '-',
-        filters: [
-          {
-            id: 'filters.orderStatus',
-            values: [
-              { id: 'TO_BE_CONFIRMED', label: this.$t('col.statuses.TO_BE_CONFIRMED') },
-              {
-                id: 'TO_BE_CONFIRMED_BY_BO',
-                label: this.$t('col.statuses.TO_BE_CONFIRMED_BY_BO'),
-              },
-              {
-                id: 'CONFIRMATION_IN_PROGRESS',
-                label: this.$t('col.statuses.CONFIRMATION_IN_PROGRESS'),
-              },
-              { id: 'CONFIRMED', label: this.$t('col.statuses.CONFIRMED') },
-            ],
-          },
-          {
-            id: 'filters.orderDate',
-            startDate: moment()
-              .subtract(6, 'month')
-              .format(dateFormat),
-            endDate: moment().format(dateFormat),
-          },
-        ],
-        fetchKey: 'ORDER_IN_PROCESS',
-        fetch: async (indicator, contextFilters) => {
-          return await fetchSingleIndicator(indicator.filters, contextFilters);
-        },
-      },
-    ];
-
-    if (!this.userIsPartner) {
-      indicators.push(
+    fetchIndicators() {
+      let indicators = [
         {
-          name: 'ordersNotConfirmed',
-          labelKey: 'indicators.getsim.ordersNotConfirmed',
-          color: 'text-warning',
+          name: 'toBeConfirmed',
+          labelKey: 'indicators.getsim.ordersToBeConfirmed',
+          color: 'text-danger',
+          clickable: true,
+          total: '-',
+          filters: [
+            {
+              id: 'filters.orderStatus',
+              values: [{ id: 'NOT_VALIDATED', label: this.$t('col.statuses.NOT_VALIDATED') }],
+            },
+            {
+              id: 'filters.orderDate',
+              startDate: moment()
+                .subtract(6, 'month')
+                .format(dateFormat),
+              endDate: moment().format(dateFormat),
+            },
+          ],
+          fetchKey: 'ORDER_TO_VALIDATE',
+          fetch: async (indicator, contextFilters) => {
+            return await fetchSingleIndicator(indicator.filters, contextFilters);
+          },
+        },
+        {
+          name: 'ordersInProgress',
+          labelKey: 'indicators.getsim.ordersInProgress',
+          color: 'text-success',
           clickable: true,
           total: '-',
           filters: [
             {
               id: 'filters.orderStatus',
               values: [
-                {
-                  id: 'TO_BE_CONFIRMED',
-                  label: this.$t('col.statuses.TO_BE_CONFIRMED'),
-                },
+                { id: 'TO_BE_CONFIRMED', label: this.$t('col.statuses.TO_BE_CONFIRMED') },
                 {
                   id: 'TO_BE_CONFIRMED_BY_BO',
                   label: this.$t('col.statuses.TO_BE_CONFIRMED_BY_BO'),
@@ -126,95 +83,140 @@ export default {
                   id: 'CONFIRMATION_IN_PROGRESS',
                   label: this.$t('col.statuses.CONFIRMATION_IN_PROGRESS'),
                 },
+                { id: 'CONFIRMED', label: this.$t('col.statuses.CONFIRMED') },
               ],
             },
             {
               id: 'filters.orderDate',
               startDate: moment()
-                .subtract(12, 'month')
-                .subtract(4, 'hours')
-                .format('DD/MM/YYYY HH:mm:ss'),
-              endDate: moment()
-                .subtract(4, 'hours')
-                .format('DD/MM/YYYY HH:mm:ss'),
-              sameDay: true,
+                .subtract(6, 'month')
+                .format(dateFormat),
+              endDate: moment().format(dateFormat),
             },
           ],
-          fetchKey: 'ORDER_NOT_CONFIRMED',
+          fetchKey: 'ORDER_IN_PROCESS',
           fetch: async (indicator, contextFilters) => {
             return await fetchSingleIndicator(indicator.filters, contextFilters);
           },
-          hideZeroValue: true,
         },
-        {
-          name: 'ordersFailed',
-          labelKey: 'indicators.getsim.ordersFailed',
-          color: 'text-warning',
-          clickable: true,
-          total: '-',
-          filters: [
-            {
-              id: 'filters.orderStatus',
-              values: [{ id: 'CONFIRMED', label: this.$t('col.statuses.CONFIRMED') }],
-            },
-            {
-              id: 'filters.orderDate',
-              startDate: moment()
-                .subtract(12, 'month')
-                .subtract(48, 'hours')
-                .format('DD/MM/YYYY HH:mm:ss'),
-              endDate: moment()
-                .subtract(48, 'hours')
-                .format('DD/MM/YYYY HH:mm:ss'),
-            },
-          ],
-          fetchKey: 'ORDER_FAILED',
-          fetch: async (indicator, contextFilters) => {
-            return await fetchSingleIndicator(indicator.filters, contextFilters);
-          },
-          hideZeroValue: true,
-        },
-        {
-          name: 'orderToBeConfirmedByBO',
-          labelKey: 'indicators.getsim.orderToBeConfirmedByBO',
-          color: 'text-danger',
-          clickable: true,
-          total: '-',
-          roles: ['BO'],
-          filters: [
-            {
-              id: 'filters.orderStatus',
-              values: [
-                {
-                  id: 'TO_BE_CONFIRMED_BY_BO',
-                  label: this.$t('col.statuses.TO_BE_CONFIRMED_BY_BO'),
-                },
-              ],
-            },
-          ],
-          fetchKey: 'ORDER_TO_VALIDATE_BO',
-          fetch: async (indicator, contextFilters) => {
-            return await fetchSingleIndicator(indicator.filters, contextFilters);
-          },
-          hideZeroValue: true,
-        },
-        {
-          name: 'averageProcessingTime',
-          labelKey: 'indicators.getsim.averageProcessingTime',
-          clickable: false,
-          total: '-',
-          filters: [],
-          fetchKey: 'ORDER_DELAY',
-          unit: true,
-        }
-      );
-    }
+      ];
 
-    this.indicators = indicators;
+      if (!this.userIsPartner) {
+        indicators.push(
+          {
+            name: 'ordersNotConfirmed',
+            labelKey: 'indicators.getsim.ordersNotConfirmed',
+            color: 'text-warning',
+            clickable: true,
+            total: '-',
+            filters: [
+              {
+                id: 'filters.orderStatus',
+                values: [
+                  {
+                    id: 'TO_BE_CONFIRMED',
+                    label: this.$t('col.statuses.TO_BE_CONFIRMED'),
+                  },
+                  {
+                    id: 'TO_BE_CONFIRMED_BY_BO',
+                    label: this.$t('col.statuses.TO_BE_CONFIRMED_BY_BO'),
+                  },
+                  {
+                    id: 'CONFIRMATION_IN_PROGRESS',
+                    label: this.$t('col.statuses.CONFIRMATION_IN_PROGRESS'),
+                  },
+                ],
+              },
+              {
+                id: 'filters.orderDate',
+                startDate: moment()
+                  .subtract(12, 'month')
+                  .subtract(4, 'hours')
+                  .format('DD/MM/YYYY HH:mm:ss'),
+                endDate: moment()
+                  .subtract(4, 'hours')
+                  .format('DD/MM/YYYY HH:mm:ss'),
+                sameDay: true,
+              },
+            ],
+            fetchKey: 'ORDER_NOT_CONFIRMED',
+            fetch: async (indicator, contextFilters) => {
+              return await fetchSingleIndicator(indicator.filters, contextFilters);
+            },
+            hideZeroValue: true,
+          },
+          {
+            name: 'ordersFailed',
+            labelKey: 'indicators.getsim.ordersFailed',
+            color: 'text-warning',
+            clickable: true,
+            total: '-',
+            filters: [
+              {
+                id: 'filters.orderStatus',
+                values: [{ id: 'CONFIRMED', label: this.$t('col.statuses.CONFIRMED') }],
+              },
+              {
+                id: 'filters.orderDate',
+                startDate: moment()
+                  .subtract(12, 'month')
+                  .subtract(48, 'hours')
+                  .format('DD/MM/YYYY HH:mm:ss'),
+                endDate: moment()
+                  .subtract(48, 'hours')
+                  .format('DD/MM/YYYY HH:mm:ss'),
+              },
+            ],
+            fetchKey: 'ORDER_FAILED',
+            fetch: async (indicator, contextFilters) => {
+              return await fetchSingleIndicator(indicator.filters, contextFilters);
+            },
+            hideZeroValue: true,
+          },
+          {
+            name: 'orderToBeConfirmedByBO',
+            labelKey: 'indicators.getsim.orderToBeConfirmedByBO',
+            color: 'text-danger',
+            clickable: true,
+            total: '-',
+            roles: ['BO'],
+            filters: [
+              {
+                id: 'filters.orderStatus',
+                values: [
+                  {
+                    id: 'TO_BE_CONFIRMED_BY_BO',
+                    label: this.$t('col.statuses.TO_BE_CONFIRMED_BY_BO'),
+                  },
+                ],
+              },
+            ],
+            fetchKey: 'ORDER_TO_VALIDATE_BO',
+            fetch: async (indicator, contextFilters) => {
+              return await fetchSingleIndicator(indicator.filters, contextFilters);
+            },
+            hideZeroValue: true,
+          },
+          {
+            name: 'averageProcessingTime',
+            labelKey: 'indicators.getsim.averageProcessingTime',
+            clickable: false,
+            total: '-',
+            filters: [],
+            fetchKey: 'ORDER_DELAY',
+            unit: true,
+          }
+        );
+      }
+      this.indicators = indicators;
+    },
+  },
+  mounted() {
+    this.fetchIndicators();
   },
   data() {
     return {
-      indicators: [],
+      indicators: undefined,
     };
   },
 };
