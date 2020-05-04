f776e39ac307836f909553947644c2541b22d5d7
Julien Pintassilgo <julien.pintassilgo@docaposte.fr>
2020-05-04 14:15:02 +0000

    FEAT : Sommaire du détail de partenaire

--- ui/src/views/GetAdmin/PartnerDetail/Summary.vue
+++ ui/src/views/GetAdmin/PartnerDetail/Summary.vue
@@ -5,37 +5,40 @@
         <div class="d-flex">
           <div class="item">
             <h6>{{ $t('getadmin.partners.name') }}:</h6>
-            <p>value</p>
+            <p>{{ getFromContent('name') }}</p>
           </div>
           <div class="item">
             <h6>{{ $t('filters.lines.siren') }}:</h6>
-            <p>value</p>
+            <p>{{ getFromContent('siren') }}</p>
           </div>
           <div class="item">
             <h6>{{ $t('filters.lines.typeSIMCard') }}:</h6>
-            <p>value</p>
+            <p>{{ getFromContent('availableSimCount') }}</p>
           </div>
           <div class="item">
             <h6>{{ $t('common.customFields') }}:</h6>
-            <p>value</p>
+            <p>{{ getFromContent('customFieldsCount') }}</p>
           </div>
         </div>
         <div class="d-flex">
           <div class="item">
             <h6>{{ $t('getadmin.partners.mainAdmin') }}:</h6>
-            <p>value</p>
+            <p>
+              {{ getFromContent('mainAdministrator.name.firstName')
+              }}{{ getFromContent('mainAdministrator.name.lastName') }}
+            </p>
           </div>
           <div class="item">
             <h6>{{ $t('getadmin.partners.users') }}:</h6>
-            <p>value</p>
+            <p>{{ getFromContent('usersCount') }}</p>
           </div>
           <div class="item">
             <h6>{{ $t('getadmin.partners.deliveryPlaces') }}:</h6>
-            <p>value</p>
+            <p>{{ getFromContent('shippingAddressesCount') }}</p>
           </div>
           <div class="item">
             <h6>{{ $t('getadmin.partners.broadcastList') }}:</h6>
-            <p>value</p>
+            <p>{{ getFromContent('mailingListsCount') }}</p>
           </div>
         </div>
       </div>
@@ -45,29 +48,29 @@
         <div class="d-flex">
           <div class="item">
             <h6>{{ $t('getadmin.partners.salesEngineer') }}:</h6>
-            <p>value</p>
+            <p>{{ getFromContent('salesEngineer') }}</p>
           </div>
           <div class="item">
             <h6>{{ $t('common.email') }}:</h6>
-            <p>value</p>
+            <p>{{ getFromContent('mainAdministrator.contactInformation.email') }}</p>
           </div>
           <div class="item">
             <h6>{{ $t('orders.new.deliveryStep.form.phone') }}</h6>
-            <p>value</p>
+            <p>{{ getFromContent('mainAdministrator.contactInformation.phone') }}</p>
           </div>
         </div>
         <div class="d-flex">
           <div class="item">
             <h6>{{ $t('getadmin.partners.lastBill') }}:</h6>
-            <p>value</p>
+            <p>-</p>
           </div>
           <div class="item">
             <h6>{{ $t('filters.offers') }}:</h6>
-            <p>value</p>
+            <p>{{ getFromContent('workflowsCount') }}</p>
           </div>
           <div class="item">
             <h6>{{ $t('common.billingAccounts') }}:</h6>
-            <p>value</p>
+            <p>{{ getFromContent('customerAccountsCount') }}</p>
           </div>
         </div>
       </div>
@@ -76,7 +79,32 @@
 </template>
 
 <script>
-export default {};
+import { fetchPartyDetail } from '@/api/partners.js';
+import get from 'lodash.get';
+
+export default {
+  props: {
+    partnerid: {
+      type: String,
+    },
+  },
+
+  data() {
+    return {
+      partyDetail: undefined,
+    };
+  },
+
+  methods: {
+    getFromContent(path, defaultValue = '-') {
+      return get(this.partyDetail, path, defaultValue);
+    },
+  },
+
+  async mounted() {
+    this.partyDetail = await fetchPartyDetail(this.partnerid);
+  },
+};
 </script>
 
 <style lang="scss" scoped>
