c351e2f782b78de328d7bd3257807c1b660975f2
Igor Trifunovic <igor.trifunovic3@docapost.fr>
2020-05-15 16:44:28 +0000

    fix: gestion des rôles pour l'administration d'utilisateur

--- ui/src/api/users.js
+++ ui/src/api/users.js
@@ -1,8 +1,17 @@
 import { query, getFilterValue, getFilterValues, getValuesIdsWithoutQuotes } from './utils';
 
-export async function fetchAllowedRoles(userId) {
+export async function fetchAllowedRoles(userId, partyId, partyGroupId) {
+  let partyGroupParam = '';
+  if (partyGroupId) {
+    partyGroupParam = `partyGroupId: ${partyGroupId},`;
+  }
+
+  let partyParam = '';
+  if (partyId) {
+    partyParam = `partyId: ${partyId},`;
+  }
   const queryStr = `query {
-    userAllowedRoles(userId: ${userId}, withWS: false) {
+    userAllowedRoles(userId: ${userId}, withWS: false, ${partyParam} ${partyGroupParam}) {
       Id
       name
       description
@@ -179,16 +188,17 @@ export async function fetchPartnerGroups(q = '') {
 export async function fetchUserRoles() {
   const queryStr = `
   query {
-    userAllowedRoles(userId: null) {
+    rolesForCurrentUser {
       Id
       name
       description
+      scope
     }
   }
   `;
 
   const response = await query(queryStr);
-  return response.data.userAllowedRoles;
+  return response.data.rolesForCurrentUser;
 }
 
 export function formatFilters(selectedFilters) {
