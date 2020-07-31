c261f0515c4887534e919b1b51e92dede6a469dc
Julien Pintassilgo <julien.pintassilgo@docaposte.fr>
2020-07-31 14:18:33 +0000

    FIX: Problème affichage historique de pays detail ligne

--- ui/src/views/GetAdmin/SearchUsers/index.vue
+++ ui/src/views/GetAdmin/SearchUsers/index.vue
@@ -9,9 +9,9 @@
         </h4>
       </div>
       <div class="col-md-3" v-if="canShow">
-        <UiButton variant="accent" block class="float-right" @click="createUserPanel()"
-          >{{ $t('getadmin.users.addUser') }}
-        </UiButton>
+        <UiButton variant="accent" block class="float-right" @click="createUserPanel()">{{
+          $t('getadmin.users.addUser')
+        }}</UiButton>
       </div>
     </div>
     <TableWithFilter
