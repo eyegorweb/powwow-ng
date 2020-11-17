8ed10cbb82f950f809b18337ba00dca0009aeb30
Yves Marie LAINE <yves-marie.laine3@docaposte.fr>
2020-11-17 09:49:06 +0000

    Add Matomo Tag Manager support

--- ui/src/main.js
+++ ui/src/main.js
@@ -8,6 +8,7 @@ import i18n from './i18n';
 import VTooltip from 'v-tooltip';
 import FeatureFlipping from './featureFlipping/plugin';
 import CommonFunctions from './utils/commonVueFunctions';
+import AnalyticsTracking from './utils/analyticsTracking';
 
 import Fragment from 'vue-fragment';
 
@@ -15,6 +16,7 @@ Vue.use(Fragment.Plugin);
 Vue.use(VTooltip);
 Vue.use(FeatureFlipping);
 Vue.use(CommonFunctions);
+Vue.use(AnalyticsTracking);
 
 Vue.config.productionTip = false;
 Vue.prototype.mina = window.mina;
@@ -23,6 +25,11 @@ import $ from 'jquery';
 import addGaugeSupport from '@/dep/GaugeMeter.js';
 
 addGaugeSupport($);
+
+router.beforeEach((to, from, next) => {
+  router.app.$pushAnalyticsIfReady({ event: 'm2m.PageView', to: { name: to.name, path: to.path } });
+  next();
+});
 
 new Vue({
   router,
