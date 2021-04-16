706b2ed08f78d699aba1a3418aef199a042ddb28
Julien Pintassilgo <julien.pintassilgo@docaposte.fr>
2021-04-16 12:57:47 +0000

    fix: [2570] Problème de permissions sur le detail de partner

--- ui/src/views/GetAdmin/PartnerDetail/AccountDetail/AccountPartnerDetail.vue
+++ ui/src/views/GetAdmin/PartnerDetail/AccountDetail/AccountPartnerDetail.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="accountdetail">
-    <div class="accountdetail-generalInfos">
+    <div class="accountdetail-generalInfos" v-if="havePermission('party', 'read_account_detail') &&
+      !(havePermission('party', 'read_main_options') ||
+        havePermission('party', 'read_secondary_options')
+      )">
       <h3>{{ $t('getadmin.partnerDetail.generalInformations') }}</h3>
       <div class="accountdetail-generalInfos-bloc">
         <div class="accountdetail-generalInfos-bloc-details">
