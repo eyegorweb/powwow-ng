ff399f8adf5d1f594764ac440470566086263f2a
Marwane <b.marwane@gmail.com>
2021-05-04 15:10:18 +0200

    fix: [2618] afficher le bouton de rechargement LV
    https://m2m-gitlab.by-docapost.com/powwow-ng/backlog/-/issues/2618

--- ui/src/views/GetParc/ActLines/FilterBar/index.vue
+++ ui/src/views/GetParc/ActLines/FilterBar/index.vue
@@ -515,10 +515,6 @@ export default {
       // 'selectedIdTypeFromFileValue',
       // 'selectedFileValue',
     ]),
-    async ipFixeEnabled() {
-      const optionsPartner = await getPartyOptions(this.userInfos.id);
-      return optionsPartner.ipFixeEnable;
-    },
     isEsimCategoryInFilter() {
       return this.selectedEsimCategoryValue === 'eSim';
     },
