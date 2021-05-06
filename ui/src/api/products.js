814f6827caed377d61e69c272fc649b36ef6f41b
Julien Pintassilgo <julien.pintassilgo@docaposte.fr>
2021-05-06 13:13:01 +0000

    feat: [1184] ajout de la gestion des cartes sim dans detail partner

--- ui/src/api/products.js
+++ ui/src/api/products.js
@@ -1,10 +1,10 @@
 import { query } from './utils';
 
-export async function fetchSim(partnerId, customerAccountId, simCategoryFilter) {
+export async function fetchSim(partnerId, customerAccountId, simCategoryFilter, allSim) {
   const response = await query(
     `
   query FindLatestSimcardsOrder($partnerId: Long!,$customerAccountId: Long, $simCategoryFilter: SIMCardCategoryFilterInput) {
-    findLatestSimcardsOrder(pagination: {page: 0, limit: 999}, sorting: {orderDate: DESC}, partyId: $partnerId, customerAccountId: $customerAccountId, SimCategoryFilter: $simCategoryFilter) {
+    findLatestSimcardsOrder(pagination: {page: 0, limit: 999}, sorting: {orderDate: DESC}, partyId: $partnerId, customerAccountId: $customerAccountId, SimCategoryFilter: $simCategoryFilter, allSimCards: ${allSim}) {
       total
       items {
         orderDate
@@ -18,6 +18,7 @@ export async function fetchSim(partnerId, customerAccountId, simCategoryFilter) 
           number
           category
         }
+        disabled
       }
     }
   }
@@ -28,11 +29,80 @@ export async function fetchSim(partnerId, customerAccountId, simCategoryFilter) 
   return response.data.findLatestSimcardsOrder.items;
 }
 
+export async function getAvailableSim(partnerId, pagination) {
+  const response = await query(
+    `
+    query{
+      getAvailableSim(partnerId: ${partnerId}, pagination: { page: ${pagination.page}, limit: ${pagination.limit}}){
+        total
+        items{
+          orderDate
+          simCard {
+            id
+            gencode
+            code
+            name
+            label
+            type
+            help
+            format
+            licence
+            number
+            electricProfile {
+              name
+            }
+            graphicProfile {
+              label
+            }
+            hardwareType {
+              name
+            }
+            category
+            description
+          }
+          disabled
+        }
+      }
+    }
+    `
+  );
+
+  return response.data.getAvailableSim.items;
+}
+
+export async function updateSimsOnParty(partnerId, simCards) {
+  const response = await query(
+    `
+    mutation {
+      updateSimsOnParty(
+        partnerId: ${partnerId}
+        simcardIds: [${simCards}]
+      )
+    }
+
+    `
+  );
+
+  return response.data.updateSimsOnParty;
+}
+
 export async function disableSimCard(partnerId, simId) {
   const response = await query(
     `
       mutation{
         disableSimForParty(partnerId: ${partnerId}, simcardId: ${simId})
+      }
+    `
+  );
+
+  return response.data.disableSimForParty;
+}
+
+export async function enableSimCard(partnerId, simId) {
+  const response = await query(
+    `
+      mutation{
+        enableSimForParty(partnerId: ${partnerId}, simcardId: ${simId})
       }
     `
   );
