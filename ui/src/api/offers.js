941fe2ca7c9723d003f2bbcedf29cd77ff9073a5
Marwane Belahcen <marwane.belahcen3@docapost.fr>
2021-05-26 08:59:19 +0000

    fix: [2690] réinitialisation des sélections lorsqu'on bascule d'une action à une autre

--- ui/src/api/offers.js
+++ ui/src/api/offers.js
@@ -27,11 +27,10 @@ export async function getAvailableOffer(partnerId, pagination) {
 `;
 
   const response = await query(queryStr);
-  console.log(response)
   if (response.data.getAvailableOffer && response.data.getAvailableOffer.items) {
     return response.data.getAvailableOffer.items;
   } else {
-    return response
+    return response;
   }
 }
 
