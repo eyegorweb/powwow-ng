67c7bfa51ec3106c664cfae6c62b495e5f24d492
Julien Pintassilgo <julien.pintassilgo@docaposte.fr>
2021-09-01 14:43:08 +0000

    feat: [1185] ajustement

--- ui/src/views/GetAdmin/PartnerDetail/OffersTab/SupervisionOptions.vue
+++ ui/src/views/GetAdmin/PartnerDetail/OffersTab/SupervisionOptions.vue
@@ -1,137 +1,147 @@
 <template>
-    <div>    
-        <table>
-            <thead>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>{{ $t('col.offer') }}</th>
+          <th>{{ $t('col.autodiag') }}</th>
+          <th>{{ $t('col.supervision') }}</th>
+          <th v-if="userIsBO">
+            <tr class="headTitle">
+              {{
+                $t('col.dmdGeoloc')
+              }}
+            </tr>
+            <tr>
+              <td>
                 <tr>
-                    <th>{{$t('col.offer')}}</th>
-                    <th>{{$t('col.autodiag')}}</th>
-                    <th>{{$t('col.supervision')}}</th>
-                    <th v-if="userIsBO">
-                        <tr class="headTitle">{{$t('col.dmdGeoloc')}}</tr>
-                        <tr>
-                            <td>
-                                <tr>
-                                    <th>{{$t('col.option')}}</th>
-                                    <th>{{$t('col.cpt')}}</th>
-                                    <th>{{$t('col.limit')}}</th>
-                                </tr>
-                            </td>
-                        </tr>
-                    </th>
-                    <th v-if="userIsBO">
-                        <tr class="headTitle">{{$t('col.requestConsoEnabled')}}</tr>
-                        <tr>
-                            <td>
-                                <tr>
-                                    <th>{{$t('col.option')}}</th>
-                                    <th>{{$t('col.cpt')}}</th>
-                                    <th>{{$t('col.limit')}}</th>
-                                </tr>
-                            </td>
-                        </tr>
-                    </th>
-                    <th v-if="!userIsBO">{{$t('col.requestConsoEnabled')}}</th>
-                    <th>IMEI</th>
-                    <th>{{$t('col.reportAvaiPDPEnabled')}}</th>
-                    <th>R-Card</th>
+                  <th>{{ $t('col.option') }}</th>
+                  <th>{{ $t('col.cpt') }}</th>
+                  <th>{{ $t('col.limit') }}</th>
                 </tr>
-            </thead>
-            <tbody>
-                <SupervisionOptionsLine v-for="row in rows" :key="row.workflowId" :row="row"/>
-            </tbody>
-        </table>
+              </td>
+            </tr>
+          </th>
 
-        <div class="modifyButton">
-            <UiButton
-                variant="primary"
-                block
-                @click="modify()"
-                >
-                    {{ $t('modify') }}
-            </UiButton>
-        </div>
+          <th v-if="userIsBO">
+            <tr class="headTitle">
+              {{
+                $t('col.requestConsoEnabled')
+              }}
+            </tr>
+            <tr>
+              <td>
+                <tr>
+                  <th>{{ $t('col.option') }}</th>
+                  <th>{{ $t('col.cpt') }}</th>
+                  <th>{{ $t('col.limit') }}</th>
+                </tr>
+              </td>
+            </tr>
+          </th>
 
+          <th v-if="!userIsBO">{{ $t('col.requestConsoEnabled') }}</th>
+          <th>IMEI</th>
+          <th>{{ $t('col.reportAvaiPDPEnabled') }}</th>
+          <th>R-Card</th>
+        </tr>
+      </thead>
+      <tbody>
+        <SupervisionOptionsLine
+          v-for="row in rows"
+          :key="row.workflowId"
+          :row="row"
+          @rCard="rCardModify"
+          @reporting="reportingModify"
+        />
+      </tbody>
+    </table>
+
+    <div class="modifyButton">
+      <UiButton variant="primary" :disabled="!canModify" block @click="modify()">
+        {{ $t('modify') }}
+      </UiButton>
     </div>
-
-</template>  
+  </div>
+</template>
 
 <script>
-import PaginatedDataTable from '@/components/DataTable/PaginatedDataTable.vue';
 import SupervisionOptionsLine from './SupervisionOptionsLine.vue';
 import { fetchSupervisionOptions } from '@/api/supervision.js';
 import UiButton from '@/components/ui/Button';
 import { mapGetters } from 'vuex';
 
 export default {
-    components: {
-        PaginatedDataTable,
-        SupervisionOptionsLine,
-        UiButton,
+  components: {
+    SupervisionOptionsLine,
+    UiButton,
+  },
+  data() {
+    return {
+      defaultOrderBy: {
+        key: 'id',
+        direction: 'DESC',
+      },
+      supervisionOptions: [],
+      columns: [],
+      rows: [],
+      canModify: false,
+    };
+  },
+  props: {
+    partner: {
+      type: Object,
     },
-    data() {
-        return {
-            defaultOrderBy: {
-                key: 'id',
-                direction: 'DESC',
-            },
-            supervisionOptions: [],
-            columns: [
-            ],
-            rows: []
-        }
+  },
+  computed: {
+    ...mapGetters(['userIsBO']),
+  },
+  async mounted() {
+    const response = await fetchSupervisionOptions(this.partner.id);
+    this.rows = response;
+  },
+  methods: {
+    rCardModify(data) {
+      console.log(`rcard ${data.id} ${data.value}`);
+      this.canModify = true;
     },
-    props: {
-        partner: {
-            type: Object,
-        },
+    reportingModify(data) {
+      console.log(`reporting ${data.id} ${data.value}`);
+      this.canModify = true;
     },
-    computed: {
-        ...mapGetters([
-        'userIsBO',
-        ]),
-    },
-    async mounted () {
-        const response = await fetchSupervisionOptions(this.partner.id);
-        this.rows = response;
-    },
-    methods: {
-        modify() {
-            
-        }
-    },
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    table {
-        width: 100%;
-        box-sizing: border-box;
-        padding: 10px;
-        
-        thead {
-            tr {
+table {
+  width: 100%;
+  box-sizing: border-box;
+  padding: 10px;
 
-                &.headTitle {
-                    border-bottom: 1px solid white;
-                }
-                
-                th {
-                    background-color: #0055a4;
-                    color: white;
-                    font-size: 0.9rem;
-                    font-weight: normal;
-                    padding: 10px;
-                    text-align: center;
-                }
-            }
-        }
-        tbody {
-            background-color: #fff;
-        }
+  thead {
+    tr {
+      &.headTitle {
+        border-bottom: 1px solid white;
+      }
+
+      th {
+        background-color: #0055a4;
+        color: white;
+        font-size: 0.9rem;
+        font-weight: normal;
+        padding: 10px;
+        text-align: center;
+      }
     }
-    .modifyButton {
-        margin-top: 10px;
-        width: 250px;
-        float: right;
-    }
+  }
+  tbody {
+    background-color: #fff;
+  }
+}
+.modifyButton {
+  margin-top: 20px;
+  width: 250px;
+  float: right;
+}
 </style>
--- ui/src/views/GetAdmin/PartnerDetail/routes.js
+++ ui/src/views/GetAdmin/PartnerDetail/routes.js
@@ -23,7 +23,8 @@ const SpecificFields = () =>
 // Offers and Sim
 const OffersCards = () => import('@/views/GetAdmin/PartnerDetail/OffersTab/OffersCards.vue');
 const SimCards = () => import('@/views/GetAdmin/PartnerDetail/OffersTab/SimCards.vue');
-const SupervisionOptions = () => import('@/views/GetAdmin/PartnerDetail/OffersTab/SupervisionOptions.vue');
+const SupervisionOptions = () =>
+  import('@/views/GetAdmin/PartnerDetail/OffersTab/SupervisionOptions.vue');
 // Account Detail
 const Options = () => import('@/views/GetAdmin/PartnerDetail/AccountDetail/PartnerOptions.vue');
 const Description = () =>
