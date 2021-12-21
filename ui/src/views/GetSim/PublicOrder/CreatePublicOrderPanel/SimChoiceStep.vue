725916828dd2f43cf038c6298eb9f691a5c8a7fd
unknown <pintassilgo.julien@gmail.com>
2021-12-21 16:31:37 +0100

    fix: [3085] max digit on simcard order digital offer

--- ui/src/views/GetSim/PublicOrder/CreatePublicOrderPanel/SimChoiceStep.vue
+++ ui/src/views/GetSim/PublicOrder/CreatePublicOrderPanel/SimChoiceStep.vue
@@ -44,6 +44,7 @@
             class="d-block w-50 mx-auto"
             input-type="number"
             :min-value="1"
+            :max-value="9999999999"
           />
           <p v-if="selectedSimIsOfTapeType" class="text-center display-nb-bobines">
             {{ $t('getsim.nb-of-sim') }}:
@@ -95,7 +96,8 @@ export default {
       return !!(
         this.selectedSimTypeValue &&
         this.selectedSimTypeValue.simCard &&
-        parseInt(this.selectedNumberOfSims)
+        parseInt(this.selectedNumberOfSims) &&
+        this.selectedNumberOfSims < 9999999999
       );
     },
   },
