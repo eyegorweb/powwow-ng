a2a0fa28a4afb80ca9bbc36c14001f7b7ce59116
Jean-Philippe QUERE <jean-philippe.quere3@docapost.fr>
2022-02-05 18:43:14 +0000

    fix: [000] Ajout log

--- ui/src/views/Authentication/AuthenticationOnLoadCallback.vue
+++ ui/src/views/Authentication/AuthenticationOnLoadCallback.vue
@@ -8,13 +8,14 @@ import { mapActions, mapMutations } from 'vuex';
 export default {
   name: 'AuthCallback',
   async mounted() {
+    console.log('AuthCallback');
     // vérifier si l'url est appelée à l'intérieur de l'iframe de refresh du token
     if (!window.frameElement) {
       // First token is loaded here
       const hashParts = this.$route.hash.split('=');
       this.setAuthToken(hashParts[1].split('&')[0]);
       await this.fetchUserInfos();
-      console.log(this.$route);
+      console.log('route': , this.$route);
       const nextRoute = this.$loGet(this.$route, 'query.prev');
       this.redirectTo(nextRoute || '/');
       this.appIsReady();
