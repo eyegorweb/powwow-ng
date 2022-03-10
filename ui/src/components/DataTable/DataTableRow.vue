a5f82a181add483e79d8b87acebeed2e5baa6649
Bacem KANNOU <bacem.kannou3@docaposte.fr>
2022-03-10 08:55:06 +0000

    fix: [3237] Le + ne doit pas s'afficher s'il n'y a pas de flux correspondant

--- ui/src/components/DataTable/DataTableRow.vue
+++ ui/src/components/DataTable/DataTableRow.vue
@@ -14,10 +14,11 @@
           @colEvent="$emit('colEvent', $event)"
         />
       </td>
-      <td v-if="subRows" @click="openSubs = !openSubs">
+      <td v-if="subRows && row.subItems && row.subItems.length !== 0" @click="openSubs = !openSubs">
         <span v-if="openSubs" class="ic-Minus-Icon cursorP" />
         <span v-else class="ic-Plus-Icon cursorP" />
       </td>
+      <td v-else></td>
       <td v-if="size">
         <slot name="actionsRow" :row="row" />
       </td>
