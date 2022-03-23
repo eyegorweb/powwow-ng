bdbbcddc3d213210299336147fd7ca6d33e3a768
Igor Trifunovic <igor.trifunovic3@docapost.fr>
2022-03-23 12:51:41 +0000

    fix: [3254] configuration de la langue utilisateur lors de la création d'offre digitale

--- ui/src/views/AccountCreation/routes.js
+++ ui/src/views/AccountCreation/routes.js
@@ -2,8 +2,6 @@ import SimStep from './SimStep.vue';
 import OfferStep from './OfferStep.vue';
 import DeliveryStep from './DeliveryStep.vue';
 import PartnerStep from './PartnerStep.vue';
-// import SuccessPage from './SuccessPage.vue';
-// import ErrorPage from './ErrorPage.vue';
 
 export default {
   path: '/create-account',
@@ -12,36 +10,26 @@ export default {
     {
       path: 'partner',
       name: 'createAccount.partner',
-      meta:{label: 'Offre digitale : Création de compte - Partenaire'},
+      meta: { label: 'Offre digitale : Création de compte - Partenaire' },
       component: PartnerStep,
     },
     {
       path: 'offer',
       name: 'createAccount.offer',
-      meta:{label: 'Offre digitale : Création de compte - Offre'},
+      meta: { label: 'Offre digitale : Création de compte - Offre' },
       component: OfferStep,
     },
     {
       path: 'sim',
       name: 'createAccount.simChoice',
-      meta:{label: 'Offre digitale : Création de compte - Sim'},
+      meta: { label: 'Offre digitale : Création de compte - Sim' },
       component: SimStep,
     },
     {
       path: 'delivery',
       name: 'createAccount.delivery',
-      meta:{label: 'Offre digitale : Création de compte - Adresse de livraison'},
+      meta: { label: 'Offre digitale : Création de compte - Adresse de livraison' },
       component: DeliveryStep,
     },
-    // {
-    //   path: 'ok/:paymentId',
-    //   name: 'createAccount.ok',
-    //   component: SuccessPage,
-    // },
-    // {
-    //   path: 'ko',
-    //   name: 'createAccount.ko',
-    //   component: ErrorPage,
-    // },
   ],
 };
