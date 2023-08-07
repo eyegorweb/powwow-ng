48857b8c6cbfbfbba7f2cf1bd363dc9d17f92a9a
Igor Trifunovic <igor.trifunovic3@docapost.fr>
2023-08-07 08:51:56 +0000

    fix: [3930] mise à jour de la visibilité du bouton d'larme Activité atypique

--- ui/src/views/GetVision/AlarmCreationPanel/AlarmChoiceButtons.vue
+++ ui/src/views/GetVision/AlarmCreationPanel/AlarmChoiceButtons.vue
@@ -97,7 +97,7 @@ export default {
         },
       ];
     } else if (
-      this.userIsPartner &&
+      (this.userIsPartner || this.userIsGroupAccount) &&
       typeof this.options.atypicalAlarm === 'boolean' &&
       this.options.atypicalAlarm
     ) {
@@ -149,7 +149,13 @@ export default {
     };
   },
   computed: {
-    ...mapGetters(['userIsAdmin', 'userIsBO', 'userIsM2MLight', 'userIsPartner']),
+    ...mapGetters([
+      'userIsAdmin',
+      'userIsBO',
+      'userIsM2MLight',
+      'userIsPartner',
+      'userIsGroupAccount',
+    ]),
     // hasOptionForInactivityAlarm() {
     //   if (this.userIsPartner) {
     //     return this.
