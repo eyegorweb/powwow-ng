f83b72655f9e01be05345d5d5956d891a801b193
Igor <igortridev@gmail.com>
2023-08-11 16:10:48 +0200

    test push credential access

--- ui/src/views/GetParc/ActLines/ActCreation/formContainers/PairingByFileFormContainer.vue
+++ ui/src/views/GetParc/ActLines/ActCreation/formContainers/PairingByFileFormContainer.vue
@@ -71,6 +71,7 @@ export default {
       if (!contextValues.tempDataUuid) {
         if (this.file) {
           response = await uploadSearchFile(this.file, 'ICCID');
+          // commentaire test
           if (!response.errors || !response.errors.length) {
             response = await this.doSendPairingRequest({
               tempDataUuid: response.tempDataUuid,
